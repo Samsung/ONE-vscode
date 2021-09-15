9888a6e2a6e70dfb2024b83d0ffe3c3e8765d0b8
OTKRyu <appn12@gmail.com>
2021-09-15 15:28:32 +0900

    refactor: apply prettier in main.js
    add semicolon in extension.ts and fix typo

--- src/extension.ts
+++ src/extension.ts
@@ -19,7 +19,7 @@ import * as vscode from 'vscode';
 import {ConfigPanel} from './Config/ConfigPanel';
 import {Project} from './Project';
 import {Utils} from './Utils';
-import { ConfigurationSettingsPanel } from './configuration-settings/ConfigurationSettingsPanel' 
+import { ConfigurationSettingsPanel } from './configuration-settings/ConfigurationSettingsPanel';
 
 export function activate(context: vscode.ExtensionContext) {
   console.log('one-vscode activate OK');
