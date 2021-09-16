b8988d88cdc3eeae198741f5f98795940c62f3dd
양준영 <wnsdud4197@naver.com>
2021-09-16 16:18:37 +0900

    fix: ReactPanel format

--- src/Visualizer/ext-src/ReactPanel.ts
+++ src/Visualizer/ext-src/ReactPanel.ts
@@ -1,5 +1,5 @@
-import * as path from "path";
-import * as vscode from "vscode";
+import * as path from 'path';
+import * as vscode from 'vscode';
 
 // export function activate(context: vscode.ExtensionContext) {
 //   context.subscriptions.push(
@@ -16,28 +16,24 @@ export class ReactPanel {
   /**
    * Track the currently panel. Only allow a single panel to exist at a time.
    */
-  public static currentPanel: ReactPanel | undefined;
+  public static currentPanel: ReactPanel|undefined;
 
-  private static readonly viewType = "react";
+  private static readonly viewType = 'react';
 
   private readonly _panel: vscode.WebviewPanel;
   private readonly _extensionPath: string;
   private _disposables: vscode.Disposable[] = [];
 
   public static createOrShow(extensionPath: string) {
-    const column = vscode.window.activeTextEditor
-      ? vscode.window.activeTextEditor.viewColumn
-      : undefined;
+    const column =
+        vscode.window.activeTextEditor ? vscode.window.activeTextEditor.viewColumn : undefined;
 
     // If we already have a panel, show it.
     // Otherwise, create a new panel.
     if (ReactPanel.currentPanel) {
       ReactPanel.currentPanel._panel.reveal(column);
     } else {
-      ReactPanel.currentPanel = new ReactPanel(
-        extensionPath,
-        column || vscode.ViewColumn.One
-      );
+      ReactPanel.currentPanel = new ReactPanel(extensionPath, column || vscode.ViewColumn.One);
     }
   }
 
@@ -45,47 +41,36 @@ export class ReactPanel {
     this._extensionPath = extensionPath;
 
     // Create and show a new webview panel
-    this._panel = vscode.window.createWebviewPanel(
-      ReactPanel.viewType,
-      "ONE vscode",
-      column,
-      {
-        // Enable javascript in the webview
-        enableScripts: true,
+    this._panel = vscode.window.createWebviewPanel(ReactPanel.viewType, 'ONE vscode', column, {
+      // Enable javascript in the webview
+      enableScripts: true,
 
-        // And restric the webview to only loading content from our extension's `media` directory.
-        localResourceRoots: [
-          vscode.Uri.file(path.join(this._extensionPath, "build"))
-        ]
-      }
-    );
+      // And restric the webview to only loading content from our extension's `media` directory.
+      localResourceRoots: [vscode.Uri.file(path.join(this._extensionPath, 'build'))]
+    });
 
     // Set the webview's initial html content
     this._panel.webview.html = this._getHtmlForWebview();
-    
+
 
     // Listen for when the panel is disposed
     // This happens when the user closes the panel or when the panel is closed programatically
     this._panel.onDidDispose(() => this.dispose(), null, this._disposables);
 
     // Handle messages from the webview
-    this._panel.webview.onDidReceiveMessage(
-      message => {
-        switch (message.command) {
-          case "alert":
-            vscode.window.showErrorMessage(message.text);
-            return;
-        }
-      },
-      null,
-      this._disposables
-    );
+    this._panel.webview.onDidReceiveMessage(message => {
+      switch (message.command) {
+        case 'alert':
+          vscode.window.showErrorMessage(message.text);
+          return;
+      }
+    }, null, this._disposables);
   }
 
   public doRefactor() {
     // Send a message to the webview webview.
     // You can send any JSON serializable data.
-    this._panel.webview.postMessage({ command: "refactor" });
+    this._panel.webview.postMessage({command: 'refactor'});
   }
 
   public dispose() {
@@ -103,23 +88,15 @@ export class ReactPanel {
   }
 
   private _getHtmlForWebview() {
-    const manifest = require(path.join(
-      this._extensionPath,
-      "build",
-      "asset-manifest.json"
-    ));
-    
-    const mainScript = manifest["files"]["main.js"];
-    const mainStyle = manifest["files"]["main.css"];
-    
-    const scriptPathOnDisk = vscode.Uri.file(
-      path.join(this._extensionPath, "build", mainScript)
-    );
-    const scriptUri = scriptPathOnDisk.with({ scheme: "vscode-resource" });
-    const stylePathOnDisk = vscode.Uri.file(
-      path.join(this._extensionPath, "build", mainStyle)
-    );
-    const styleUri = stylePathOnDisk.with({ scheme: "vscode-resource" });
+    const manifest = require(path.join(this._extensionPath, 'build', 'asset-manifest.json'));
+
+    const mainScript = manifest['files']['main.js'];
+    const mainStyle = manifest['files']['main.css'];
+
+    const scriptPathOnDisk = vscode.Uri.file(path.join(this._extensionPath, 'build', mainScript));
+    const scriptUri = scriptPathOnDisk.with({scheme: 'vscode-resource'});
+    const stylePathOnDisk = vscode.Uri.file(path.join(this._extensionPath, 'build', mainStyle));
+    const styleUri = stylePathOnDisk.with({scheme: 'vscode-resource'});
 
     // Use a nonce to whitelist which scripts can be run
     const nonce = getNonce();
@@ -132,10 +109,11 @@ export class ReactPanel {
 				<meta name="theme-color" content="#000000">
 				<title>React App</title>
 				<link rel="stylesheet" type="text/css" href="${styleUri}">
-				<meta http-equiv="Content-Security-Policy" content="default-src *; img-src vscode-webview: vscode-resource: https: data: http: blob:; script-src 'nonce-${nonce}';style-src vscode-resource: 'unsafe-inline' http: https: data:;">
-        <base href="${vscode.Uri.file(path.join(this._extensionPath, "build")).with({
-          scheme: "vscode-resource"
-        })}/">
+				<meta http-equiv="Content-Security-Policy" content="default-src *; img-src vscode-webview: vscode-resource: https: data: http: blob:; script-src 'nonce-${
+        nonce}';style-src vscode-resource: 'unsafe-inline' http: https: data:;">
+        <base href="${vscode.Uri.file(path.join(this._extensionPath, 'build')).with({
+      scheme: 'vscode-resource'
+    })}/">
 			</head>
 
 			<body>
@@ -149,9 +127,8 @@ export class ReactPanel {
 }
 
 function getNonce() {
-  let text = "";
-  const possible =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let text = '';
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   for (let i = 0; i < 32; i++) {
     text += possible.charAt(Math.floor(Math.random() * possible.length));
   }
