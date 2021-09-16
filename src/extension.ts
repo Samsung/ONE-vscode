55a89ad075a7fb3e43760882aed5bfa5735f030a
양준영 <wnsdud4197@naver.com>
2021-09-16 14:29:27 +0900

    fix: path setting

--- src/extension.ts
+++ src/extension.ts
@@ -40,7 +40,7 @@ export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(disposableOneImport);
   
   let disposalbeOneVisualizer = vscode.commands.registerCommand("one-vscode-barchart.start", () => {
-    ReactPanel.createOrShow(context.extensionPath+'\\src\\Visualizer');
+    ReactPanel.createOrShow(context.extensionPath+'/src/Visualizer');
   });
   context.subscriptions.push(disposalbeOneVisualizer);
   
