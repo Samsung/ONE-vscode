450439db02d0b992fda0214ac4ec21716fc41e2b
양준영 <wnsdud4197@naver.com>
2021-09-16 11:40:10 +0900

    fix: react lint

--- src/extension.ts
+++ src/extension.ts
@@ -49,13 +49,3 @@ export function activate(context: vscode.ExtensionContext) {
 export function deactivate() {
   // TODO do cleanup
 }
-
-function getNonce() {
-  let text = "";
-  const possible =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  for (let i = 0; i < 32; i++) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-  }
-  return text;
-}
