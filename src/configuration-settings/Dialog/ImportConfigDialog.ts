6656f2c364a148335ec21bd348dd46c3296c1795
sj <asj12896@gmail.com>
2021-09-16 11:50:50 +0900

    fix: fix config file path name ONE-vscode-DCO-1.0-Signed-off-by: sunjin an asj12896@gmail.com

--- src/configuration-settings/Dialog/ImportConfigDialog.ts
+++ src/configuration-settings/Dialog/ImportConfigDialog.ts
@@ -11,12 +11,12 @@ export function importConfig(newWebview: vscode.Webview): void{
       };
       vscode.window.showOpenDialog(optionsImport).then((fileUri) => {
         if (fileUri && fileUri[0]) {
-          const pathToModelFile = fileUri[0].fsPath;
+          const pathToConfiglFile = fileUri[0].fsPath;
 
           const configParser = require("configparser");
           const config = new configParser();
 
-          config.read(pathToModelFile);
+          config.read(pathToConfiglFile);
           const sections = config.sections();
           const options = sections.reduce(
             (options: object, section: string) => ({
@@ -27,7 +27,7 @@ export function importConfig(newWebview: vscode.Webview): void{
           );
         newWebview.postMessage({
             command: "importConfig",
-            filePath: pathToModelFile,
+            filePath: pathToConfiglFile,
             options: options,
           });
         }
