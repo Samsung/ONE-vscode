981df2a37ec17f031adad8294a7a44b0566b9009
sj <asj12896@gmail.com>
2021-09-16 10:58:26 +0900

    feat: add import Configuration ONE-vscode-DCO-1.0-Signed-off-by: sunjin an asj12896@gmail.com

---
+++ src/configuration-settings/Dialog/ImportConfigDialog.ts
@@ -0,0 +1,35 @@
+import * as vscode from 'vscode';
+
+export function importConfig(newWebview: vscode.Webview): void{
+    const optionsImport: vscode.OpenDialogOptions = {
+        canSelectMany: false,
+        openLabel: "Import",
+        filters: {
+          /* eslint-disable-next-line @typescript-eslint/naming-convention */
+          "ONE .cfg Files": ["cfg"],
+        },
+      };
+      vscode.window.showOpenDialog(optionsImport).then((fileUri) => {
+        if (fileUri && fileUri[0]) {
+          const pathToModelFile = fileUri[0].fsPath;
+
+          const configParser = require("configparser");
+          const config = new configParser();
+
+          config.read(pathToModelFile);
+          const sections = config.sections();
+          const options = sections.reduce(
+            (options: object, section: string) => ({
+              ...options,
+              [section]: config.items(section),
+            }),
+            {}
+          );
+        newWebview.postMessage({
+            command: "importConfig",
+            filePath: pathToModelFile,
+            options: options,
+          });
+        }
+      });
+} 
