2f3803d469bcf5257c0477582b4909a34aa3c1ad
TaeKyuRyu <appn12@gmail.com>
2021-09-16 16:45:26 +0900

    feat: final change before pull request
    so many changes

--- src/configuration-settings/Dialog/ExportConfigDialog.ts
+++ src/configuration-settings/Dialog/ExportConfigDialog.ts
@@ -1,39 +1,49 @@
 import * as vscode from "vscode";
 
-export function exportConfig(oneToolList: any): void{
-    const configPareser = require("configparser");
-    const config = new configPareser();
-    
-    config.addSection("one-build");
+export function exportConfig(oneToolList: any): void {
+  const configPareser = require("configparser");
+  const config = new configPareser();
 
-    for(let i = 0; i < oneToolList.length; i++){
-      config.set("one-build", oneToolList[i].type, oneToolList[i].use ? "True":"False");
-      if(oneToolList[i].use === false) {
+  config.addSection("one-build");
+
+  for (let i = 0; i < oneToolList.length; i++) {
+    config.set(
+      "one-build",
+      oneToolList[i].type,
+      oneToolList[i].use ? "True" : "False"
+    );
+    if (oneToolList[i].use === false) {
+      continue;
+    }
+    config.addSection(oneToolList[i].type);
+    for (let j = 0; j < oneToolList[i].options.length; j++) {
+      let optionValue = oneToolList[i].options[j].optionValue;
+      if (optionValue === false || optionValue === "") {
         continue;
       }
-      config.addSection(oneToolList[i].type);
-      for(let j = 0; j < oneToolList[i].options.length; j++){
-        let optionValue = oneToolList[i].options[j].optionValue;
-        if(optionValue === false || optionValue === "") {
-          continue;
-        }
-        if(typeof optionValue === "boolean") {
-          optionValue = "True";
-        }
-        config.set(oneToolList[i].type, oneToolList[i].options[j].optionName, optionValue);
+      if (typeof optionValue === "boolean") {
+        optionValue = "True";
       }
+      config.set(
+        oneToolList[i].type,
+        oneToolList[i].options[j].optionName,
+        optionValue
+      );
     }
+  }
 
-    const optionsForExportDialog: vscode.SaveDialogOptions = {
-        defaultUri: vscode.Uri.file("one-build-template.cfg"),
-        filters: {
-          "allFiles": ["*"]
-        }
-      };
-    vscode.window.showSaveDialog(optionsForExportDialog).then(fileUri => {
-      if (fileUri) {
-        config.write(fileUri.path);
-        console.log("Selected file: " + fileUri.path);
-        }
-    });
+  const optionsForExportDialog: vscode.SaveDialogOptions = {
+    defaultUri: vscode.Uri.file("one-build-template.cfg"),
+    filters: {
+      allFiles: ["*"],
+    },
+  };
+  vscode.window.showSaveDialog(optionsForExportDialog).then((fileUri) => {
+    if (fileUri) {
+      config.write(fileUri.path);
+      vscode.window.showInformationMessage(
+        "Your configuration file is successfully generated!"
+      );
+    }
+  });
 }
