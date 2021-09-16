45ecfe03779303649abbba0dae9bc49a87fc404c
yeeybook <yeeybook@gmail.com>
2021-09-16 16:04:03 +0900

    feat: add export configuration file

---
+++ src/configuration-settings/Dialog/InputFileDialog.ts
@@ -0,0 +1,24 @@
+import * as vscode from 'vscode';
+
+export function getInputPath(webview: vscode.Webview, selectedTool: string): void{
+  const optionsForInputDialog: vscode.OpenDialogOptions = {
+    canSelectMany: false, 
+    openLabel: "Open",
+    filters: {
+      "allFiles": ["*"]
+  }
+  };
+  
+  vscode.window.showOpenDialog(optionsForInputDialog).then(fileUri => {
+  if (fileUri && fileUri[0]) {
+    const pathToModelFile = fileUri[0].fsPath;
+    console.log("Selected file: " + pathToModelFile);
+        webview.postMessage({
+          command: "inputPath",
+          selectedTool: selectedTool,
+          filePath: pathToModelFile,
+        });
+      }
+    }
+  );
+}
