d1780ee859b13e375f04b2d70182f889902a6da0
parkjisu6239 <parkjisu6239@gmail.com>
2021-09-23 10:21:44 +0900

    feat: refactoring and fix zoom
    add semi colon and zoom ratio

--- src/Visualizer/bar.js
+++ src/Visualizer/bar.js
@@ -1,78 +1,73 @@
-import { renderSingleDetail, renderMultipleDetail } from './detail.js'
+import { renderSingleDetail, renderMultipleDetail } from './detail.js';
 
-export default function renderBar(endTime, digit, data){
-    const barLists = document.querySelectorAll('.barList')
-    const barList = barLists[barLists.length - 1]
+export default function renderBar(endTime, data){
+    const barLists = document.querySelectorAll('.bar-list');
+    const barList = barLists[barLists.length - 1];
 
-    const bar = document.createElement('div')
-    bar.className = "bar"
-    bar.style.left = `${data.ts/endTime*100}%`
-    bar.style.width = `${data.dur/endTime*100}%`
-    bar.style.backgroundColor = `${data.backgroundColor}`
+    const bar = document.createElement('div');
+    bar.className = "bar";
+    bar.style.left = `${data.ts/endTime*100}%`;
+    bar.style.width = `${data.dur/endTime*100}%`;
+    bar.style.backgroundColor = `${data.backgroundColor}`;
 
     Object.keys(data).map(key => {
         if (key === 'args') {
-            const args = data[key]
-            const argList = Object.keys(args).map((key, idx) => {
-                return `${key} : ${args[key]}`
-            })
-            bar.dataset['args'] = argList.join('.#/#.')
+            const args = data[key];
+            const argList = Object.keys(args).map(key => {
+                return `${key} : ${args[key]}`;
+            });
+            bar.dataset['args'] = argList.join('.#/#.');
         } else {
-            bar.dataset[`${key}`] = data[key]           
+            bar.dataset[`${key}`] = data[key];       
         }
-    })
+    });
 
     bar.addEventListener('click', e => {
-        removeDetail()
+        removeDetail();
         if (e.ctrlKey) {
-            addSelectedBar(e.target.className === 'bar' ? e.target : e.target.parentNode)
-            renderMultipleDetail()    
+            addSelectedBar(e.target.className === 'bar' ? e.target : e.target.parentNode);
+            renderMultipleDetail();
         } else {
-            removeSelectedBar()
-            renderSingleDetail(e.target.className === 'bar' ? e.target.dataset : e.target.parentNode.dataset)
+            removeSelectedBar();
+            renderSingleDetail(e.target.className === 'bar' ? e.target.dataset : e.target.parentNode.dataset);
         }
-    })
+    });
     
-    const barTitle = document.createElement('div')
-    barTitle.className = "barTitle"
-    barTitle.innerText = data['name']
+    const barTitle = document.createElement('div');
+    barTitle.className = "bar-title";
+    barTitle.innerText = data['name'];
 
-    bar.append(barTitle)
-    barList.append(bar)   
+    bar.append(barTitle);
+    barList.append(bar);
 }
 
 function addSelectedBar(ele) {
-    const selectedOpList = document.querySelectorAll('.selectedOp')
+    const selectedOpList = document.querySelectorAll('.selected-op');
     for (const selectedOp of selectedOpList) {
         if (selectedOp.dataset['pk'] === ele.dataset['pk']) {
             return;
         }
     }
 
-    const selected = document.querySelector('.selected')
-    const op = document.createElement('div')
-    op.className = 'selectedOp'
+    const selected = document.querySelector('.selected');
+    const op = document.createElement('div');
+    op.className = 'selected-op';
     Object.keys(ele.dataset).forEach(key => {
-        op.dataset[key] = ele.dataset[key]
-    })
-    selected.append(op)
+        op.dataset[key] = ele.dataset[key];
+    });
+    selected.append(op);
 }
 
 function removeSelectedBar() {
-    const selected = document.querySelector('.selected')
+    const selected = document.querySelector('.selected');
     while(selected.hasChildNodes()) {
         selected.removeChild(selected.firstChild);
     }
 }
 
 function removeDetail() {
-    const table = document.querySelector('.detail-container section table')
-    if (table) {
-        table.remove()
-    }
-
-    const ul = document.querySelector('.detail-container section ul')
-    if (ul) {
-        ul.remove()
-    }
+    const details = document.querySelectorAll('.detail');
+    details.forEach(detail => {
+        detail.remove();
+    });
 }
--- src/Visualizer/dashboard.js
+++ src/Visualizer/dashboard.js
@@ -1,10 +1,10 @@
-import renderRuler from './ruler.js'
-import renderLevel from './level.js'
+import renderRuler from './ruler.js';
+import renderLevel from './level.js';
 
 export default function renderDashboard(utility, endTime, digit, data){
-    renderRuler(endTime, digit)
+    renderRuler(endTime, digit);
     Object.keys(data).map(key => {
-        renderLevel(endTime, digit, key, utility[key], data[key])             
-    })
+        renderLevel(endTime, key, utility[key], data[key]);       
+    });
     
 }
