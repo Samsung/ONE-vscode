33d239028acad04e8eb50205b61414c71a63eb7a
wnsdud4197 <53335105+wnsdud4197@users.noreply.github.com>
2021-09-27 17:10:23 +0900

    Update src/Jsontracer/GetWebviewOptions.ts
    Co-authored-by: Seok Namkoong <seok9311@naver.com>

--- src/Jsontracer/GetWebviewOptions.ts
+++ src/Jsontracer/GetWebviewOptions.ts
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 import * as vscode from 'vscode';
 
 export function getWebviewOptions(extensionUri: vscode.Uri): vscode.WebviewOptions {
