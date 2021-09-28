23393dd5d23c750df94d7284d36863d3bbd1fee2
ysys <emoney96@naver.com>
2021-09-28 23:21:38 +0900

    [Editor] Introduce hover
    This will provide a hover to help explain the tool in ini language.
    
    ONE-vscode-DCO-1.0-Signed-off-by: ysys <emoney96@naver.com>

---
+++ src/Editor/HoverProvider.ts
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2021 Samsung Electronics Co., Ltd. All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as vscode from 'vscode';
+
+export class HoverProvider implements vscode.HoverProvider {
+  provideHover(_doc: { getWordRangeAtPosition: (arg0: any, arg1: RegExp) => any; getText: (arg0: any) => any; }, _position: any, _token: any) {
+    // TODO implement
+    return null;
+  }
+}
