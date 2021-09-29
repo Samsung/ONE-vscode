84c2b5c89caa5728f60b996b670b8e8fb0595d10
sj <asj12896@gmail.com>
2021-09-29 14:37:22 +0900

    [Config] Enable import for webview
    This will enable webivew to import cfg file
    
    ONE-vscode-DCO-1.0-Signed-off-by: sunjin an asj12896@gmail.com

---
+++ media/Config/importConfig.js
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2021 Samsung Electronics Co., Ltd. All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const oneImportTools = function(data, importOpt, tool, idx, defaultImportObject) {
+  oneImport.use = true;
+  for (let i = 0; i < defaultImportObject.options.length; i++) {
+    if (importOpt === defaultImportObject.options[i].optionName) {
+      defaultImportObject.options[i].optionValue = data[tool][importOpt];
+    }
+  }
+  for (let i = 0; i < oneImport.options.length; i++) {
+    if (i === idx) {
+      oneImport.options[i].optionValue = true;
+    } else {
+      oneImport.options[i].optionValue = false;
+    }
+  }
+};
+
+const oneOtherTools = function(data, importOpt, tool, otherTool) {
+  for (let i = 0; i < otherTool.options.length; i++) {
+    if (importOpt === otherTool.options[i].optionName && data[tool][importOpt] === 'False') {
+      otherTool.options[i].optionValue = false;
+    } else if (importOpt === otherTool.options[i].optionName && data[tool][importOpt] === 'True') {
+      otherTool.options[i].optionValue = true;
+    } else if (importOpt === otherTool.options[i].optionName) {
+      otherTool.options[i].optionValue = data[tool][importOpt];
+    }
+  }
+};
