82be1ccc9da9f1bbd68273c6bf01daa078b05724
jisu <42528189+parkjisu6239@users.noreply.github.com>
2021-09-29 10:55:08 +0900

    [Jsontracer] Add dashboard to render bar-graph (#167)
    * [Jsontracer] Add dashboard to render bar-graph
    
    This will render the ruler and the level.
    
    ONE-vscode-DCO-1.0-Signed-off-by: Jisu Park <parkjisu6239@gmail.com>
    
    * Apply format-checker for changes

--- media/Jsontracer/dashboard.js
+++ media/Jsontracer/dashboard.js
@@ -44,88 +44,12 @@
 
 // This file referenced the result of https://github.com/catapult-project/catapult/tree/444aba89e1c30edf348c611a9df79e2376178ba8/tracing
 
-import renderDashboard from './dashboard.js';
-import getColorList from "./colorList.js";
+// TODO import renderRuler from './ruler.js';
+// TODO import renderLevel from './level.js';
 
-const setData = document.querySelector(".set-data");
-
-export default function openFileSelector() {
-  const input = document.createElement("input");
-  input.type = "file";
-  input.accept = "text/plain";
-  input.onchange = (event) => {
-    setFileName(event.target.files[0].name);
-    processFile(event.target.files[0]);
-  };
-  input.click();
-}
-
-function setFileName(name) {
-  const fileName = document.querySelector(".file-name");
-  fileName.innerText = name;
-}
-
-function processFile(file) {
-  const reader = new FileReader();
-  reader.onload = () => {
-    const data = JSON.parse(reader.result);
-    processData(data.traceEvents);
-
-    // set data to DOM
-    setData.dataset["displayTimeUnit"] = data.displayTimeUnit;
-  };
-  reader.readAsText(file, "euc-kr");
-}
-
-function processData(data) {
-  const processedData = {};
-  const backgroundColor = {};
-  const utility = {};
-  const colorList = getColorList();
-  let endTime = 0;
-  let colorIdx = 0;
-
-  data.forEach((ele, idx) => {
-    // exception handling
-    if (!ele.name) {
-      return;
-    }
-
-    // set data object
-    if (processedData[ele.pid] === undefined) {
-      processedData[ele.pid] = {};
-    }
-    if (processedData[ele.pid][ele.tid] === undefined) {
-      processedData[ele.pid][ele.tid] = [];
-    }
-
-    // set backgroud-color
-    if (!backgroundColor[ele.name]) {
-      backgroundColor[ele.name] = colorList[colorIdx];
-      colorIdx += 1;
-      colorIdx %= colorList.length;
-    }
-
-    endTime = Math.max(endTime, ele.ts + ele.dur);
-    utility[ele.pid] = utility[ele.pid] ? utility[ele.pid] + ele.dur : ele.dur;
-
-    ele["pk"] = idx;
-    ele["backgroundColor"] = backgroundColor[ele.name];
-
-    processedData[ele.pid][ele.tid].push(ele);
+export default function renderDashboard(utility, timeLimit, digit, data) {
+  // TODO renderRuler(timeLimit, digit);
+  Object.keys(data).map(key => {
+    // TODO renderLevel(timeLimit, key, utility[key], data[key]);
   });
-
-  Object.keys(utility).forEach(key => {
-    utility[key] = Math.round((utility[key] * 100) / endTime) / 100;
-  });
-
-  const digit = parseInt(endTime).toString().length;
-  const timeLimit = Math.ceil(endTime / 10 ** (digit - 1)) * 10 ** (digit - 1);
-
-  // set data to DOM
-  setData.dataset["digit"] = digit;
-  setData.dataset["timeLimit"] = timeLimit;
-
-  // render dashboard
-  renderDashboard(utility, timeLimit, digit, processedData);
 }
