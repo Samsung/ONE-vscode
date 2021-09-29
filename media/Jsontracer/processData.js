02255f0b99e338bdc49f08f84500f420592f55d4
jisu <42528189+parkjisu6239@users.noreply.github.com>
2021-09-29 10:42:27 +0900

    Apply format-checker for changes

--- media/Jsontracer/processData.js
+++ media/Jsontracer/processData.js
@@ -44,7 +44,7 @@
 
 // This file referenced the result of https://github.com/catapult-project/catapult/tree/444aba89e1c30edf348c611a9df79e2376178ba8/tracing
 
-import renderDashboard from "./dashboard.js";
+import renderDashboard from './dashboard.js';
 import getColorList from "./colorList.js";
 
 const setData = document.querySelector(".set-data");
