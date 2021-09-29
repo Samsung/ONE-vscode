28892d2a99be897a39a9ab835a0a859126775231
jisu <42528189+parkjisu6239@users.noreply.github.com>
2021-09-29 12:54:30 +0900

    check-format

--- media/Jsontracer/dashboard.js
+++ media/Jsontracer/dashboard.js
@@ -50,7 +50,8 @@ import renderLevel from './level.js';
 
 export default function renderDashboard(utility, timeLimit, digit, data) {
   // TODO renderRuler(timeLimit, digit);
-  Object.keys(data).map(key => {
-    renderLevel(timeLimit, key, utility[key], data[key]);
-  });
+  Object.keys(data).map(
+    key => {
+        renderLevel(timeLimit, key, utility[key], data[key]);
+    });
 }
