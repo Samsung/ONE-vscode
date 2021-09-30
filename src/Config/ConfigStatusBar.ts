edfabb8ce2ae39c1a77c5516b0048272b5c6aa9c
sj <asj12896@gmail.com>
2021-09-30 10:46:30 +0900

    update src/Config/ConfigStatusBar.ts

--- src/Config/ConfigStatusBar.ts
+++ src/Config/ConfigStatusBar.ts
@@ -17,10 +17,10 @@
 import * as vscode from 'vscode';
 
 export function createStatusBarItem(context: vscode.ExtensionContext) {
-  let myStatusBarItem: vscode.StatusBarItem;
-  myStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
-  myStatusBarItem.text = `$(file-add) ONE configuration Settings`;
-  myStatusBarItem.command = 'onevscode.configuration-settings';
-  context.subscriptions.push(myStatusBarItem);
-  myStatusBarItem.show();
+  let cfgStatusBarItem: vscode.StatusBarItem;
+  cfgStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+  cfgStatusBarItem.text = `$(file-add) ONE configuration Settings`;
+  cfgStatusBarItem.command = 'onevscode.configuration-settings';
+  context.subscriptions.push(cfgStatusBarItem);
+  cfgStatusBarItem.show();
 }
