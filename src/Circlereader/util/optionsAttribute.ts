5be538d430ea68b4c6d1d1628004c6b4a4a77d53
code1995 <code.57x53@gmail.com>
2021-09-30 01:58:49 -0700

    add license

--- src/Circlereader/util/optionsAttribute.ts
+++ src/Circlereader/util/optionsAttribute.ts
@@ -1,5 +1,20 @@
-import * as flatbuffers from 'flatbuffers';
+/*
+ * Copyright (c) 2021 Samsung Electronics Co., Ltd. All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
+import * as flatbuffers from 'flatbuffers';
 import {ActivationFunctionType} from '../circle-analysis/circle/activation-function-type'
 import {AddOptions} from '../circle-analysis/circle/add-options';
 import {ArgMaxOptions} from '../circle-analysis/circle/arg-max-options';
