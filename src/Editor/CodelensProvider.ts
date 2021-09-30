1f8c75501c3ab54de9a8c02be8d7d42331492002
Seojeonguk <uk7880@naver.com>
2021-09-30 15:19:25 +0900

    remove unnecessary variable

--- src/Editor/CodelensProvider.ts
+++ src/Editor/CodelensProvider.ts
@@ -61,7 +61,6 @@ export class CodelensProvider implements vscode.CodeLensProvider {
   public resolveCodeLens(codeLens: vscode.CodeLens, token: vscode.CancellationToken) {
     // TODO implement
     let lineStr = vscode.window.activeTextEditor ?.document.getText(codeLens.range);
-    let nowToolName = '';
 
     if (lineStr?.indexOf('=') === -1) {
       toolsAttr.forEach((tool) => {
