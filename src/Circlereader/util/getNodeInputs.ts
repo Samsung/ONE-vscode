7da9bc7ecd6a4e04ce6c7950ca2ad47bacaf388c
code1995 <code.57x53@gmail.com>
2021-09-30 01:37:15 -0700

    Fix due to format checker

--- src/Circlereader/util/getNodeInputs.ts
+++ src/Circlereader/util/getNodeInputs.ts
@@ -1,19 +1,3 @@
-/*
- * Copyright (c) 2021 Samsung Electronics Co., Ltd. All Rights Reserved
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import {Model} from '../circle-analysis/circle/model';
 import {Operator} from '../circle-analysis/circle/operator';
 import {SubGraph} from '../circle-analysis/circle/sub-graph';
@@ -26,13 +10,13 @@ export function getNodeInputs(
   let inputLength = operator.inputsLength();
 
   for (let inputIdx = 0; inputIdx < inputLength; inputIdx++) {
-        let node_input: NodeInput = {
+        let nodeInput: NodeInput = {
             location: inputArr[inputIdx],
             name: nodesArr[inputArr[inputIdx]]['name'],
             type: nodesArr[inputArr[inputIdx]]['type'],
             edge: model.buffers(subgraph.tensors(inputArr[inputIdx])?.buffer()!)?.dataLength() == 0 ? true : false
         };
-        nodeInputs.push(node_input);
+        nodeInputs.push(nodeInput);
   }
 
   return nodeInputs;
