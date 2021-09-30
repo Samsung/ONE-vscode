42a86e243fe7778d64d11327517ca00b2c93eb36
code1995 <code.57x53@gmail.com>
2021-09-30 01:44:58 -0700

    add semicolon

--- src/Circlereader/type/types.d.ts
+++ src/Circlereader/type/types.d.ts
@@ -16,30 +16,30 @@
 
 declare type Node = {
   name: String; type: Int32Array;
-}
+};
 
 declare type NodeProperties = {
   type: String,
   location: Number
-}
+};
 
 declare type NodeAttributes = {
   attribute: String,
   value: string | Number | Boolean | Int32Array | null
-}
+};
 
 declare type NodeInput = {
   location: Number,
   name: String,
   type: Int32Array,
   edge: Boolean
-}
+};
 
 declare type NodeOutput = {
   location: Number,
   name: String,
   type: Int32Array
-}
+};
 
 declare type ModelOperator = {
   builtinoptions: String,
@@ -47,8 +47,8 @@ declare type ModelOperator = {
   attributes: Array<NodeAttributes>,
   inputs: Array<NodeInput>,
   outputs: Array<NodeOutput>
-}
+};
 
 export {
   Node, NodeAttributes, NodeInput, NodeOutput, NodeProperties, ModelOperator
-}
+};
