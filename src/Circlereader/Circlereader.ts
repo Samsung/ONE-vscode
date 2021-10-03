16aface0e8cd2cefbee9b4cf9f96c6336adf92e8
yesfordev <yesfordev@gmail.com>
2021-10-03 21:50:06 +0900

    Change variable name and code in Circlereader.ts

--- src/Circlereader/Circlereader.ts
+++ src/Circlereader/Circlereader.ts
@@ -16,14 +16,15 @@
 
 import * as flatbuffers from 'flatbuffers';
 import {readFileSync} from 'fs';
-import {BuiltinOptions} from './circle-analysis/circle/builtin-options'
+
+import {BuiltinOptions} from './circle-analysis/circle/builtin-options';
 import {Model} from './circle-analysis/circle/model';
-import {ModelOperator, Node, NodeAttributes, NodeInput, NodeOutput, NodeProperties} from './type/types'
-import {setAttributesByOption} from './util/setAttributesByOption';
+import {ModelOperator, Node, NodeAttributes, NodeInput, NodeOutput, NodeProperties} from './type/types';
 import {getNodeInputs} from './util/getNodeInputs';
 import {getNodeOutputs} from './util/getNodeOutputs';
 import {initBuiltInOperator} from './util/initBuiltinOptions';
 import {initSubgraphNodes} from './util/initSubgraphNodes';
+import {setAttributesByOption} from './util/setAttributesByOption';
 
 export function decoder(path: string) {
   const file = readFileSync(path);
@@ -36,47 +37,46 @@ export function decoder(path: string) {
   let builtInOperatorArr: Array<String> = initBuiltInOperator(model);
 
   // init subgraphs
-  let subgraph = model.subgraphs(0);
+  let subgraph = model.subgraphs(0)!;
 
-  if (subgraph != null) {
-    for (let subgraphIdx = 0; subgraphIdx < subgraphsLength; subgraphIdx++) {
-      // indirect bb_pos to next subgraph index
-      model.subgraphs(subgraphIdx, subgraph);
+  for (let subgraphIdx = 0; subgraphIdx < subgraphsLength; subgraphIdx++) {
+    // indirect bb_pos to next subgraph index
+    model.subgraphs(subgraphIdx, subgraph);
 
-      let nodesArr: Array<Node> = initSubgraphNodes(subgraph);  // init node array
+    let nodesArr: Array<Node> = initSubgraphNodes(subgraph);  // init node array
 
-      // The inputs and outputs from the operator mean the location of the tensor
-      for (let operatorIdx = 0; operatorIdx < subgraph.operatorsLength(); operatorIdx++) {
-        let operator = subgraph.operators(operatorIdx)!;
-        let modelType = builtInOperatorArr[operator.opcodeIndex()];
-        let modelLocation = operatorIdx;
-        let modelProp: NodeProperties = {type: modelType, location: modelLocation};
+    // The inputs and outputs from the operator mean the location of the tensor
+    for (let operatorIdx = 0; operatorIdx < subgraph.operatorsLength(); operatorIdx++) {
+      let operator = subgraph.operators(operatorIdx)!;
+      let operatorType = builtInOperatorArr[operator.opcodeIndex()];
+      let operatorLocation = operatorIdx;
+      let operatorProp: NodeProperties = {type: operatorType, location: operatorLocation};
 
-        // get node attribute
-        let option = BuiltinOptions[operator.builtinOptionsType()];
-        let modelAttribute: Array<NodeAttributes> = [];
+      // get node attribute
+      let option = BuiltinOptions[operator.builtinOptionsType()];
+      let operatorAttribute: Array<NodeAttributes> = [];
 
-        setAttributesByOption(option, operator, modelAttribute);
+      setAttributesByOption(option, operator, operatorAttribute);
 
-        // get node inputs
-        let nodeInputs: Array<NodeInput> = getNodeInputs(model, subgraph, operator, nodesArr);
-        // get node outputs
-        let nodeOutputs: Array<NodeOutput> = getNodeOutputs(operator, nodesArr);
-        // get builtin-option of node
-        let builtinoptions = BuiltinOptions[operator.builtinOptionsType()];
+      // get node inputs
+      let nodeInputs: Array<NodeInput> = getNodeInputs(model, subgraph, operator, nodesArr);
+      // get node outputs
+      let nodeOutputs: Array<NodeOutput> = getNodeOutputs(operator, nodesArr);
+      // get builtin-option of node
+      let builtinoptions = BuiltinOptions[operator.builtinOptionsType()];
 
-        let modelOper: ModelOperator = {
-          builtinoptions: builtinoptions,
-          properties: modelProp,
-          attributes: modelAttribute,
-          inputs: nodeInputs,
-          outputs: nodeOutputs
-        };
+      let modelOper: ModelOperator = {
+        builtinoptions: builtinoptions,
+        properties: operatorProp,
+        attributes: operatorAttribute,
+        inputs: nodeInputs,
+        outputs: nodeOutputs
+      };
 
-        resultArr.push(modelOper);
-      }
+      resultArr.push(modelOper);
     }
   }
+
   let jsondata = JSON.stringify(resultArr);
   return jsondata;
 };
