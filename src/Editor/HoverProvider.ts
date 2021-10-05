29f6e1827211b3c0ea4f1daa62f69ff7a00270ca
YongSoo Kang <60142959+emost22@users.noreply.github.com>
2021-10-05 16:05:54 +0900

    [Editor] Fix the conditions that show the Option List. (#245)
    This will show Option List as a MarkdownString only for tools with sub-attributes.
    
    ONE-DCO-1.0-Signed-off-by: YongSoo Kang <emoney96@naver.com>

--- src/Editor/HoverProvider.ts
+++ src/Editor/HoverProvider.ts
@@ -34,7 +34,9 @@ export class HoverProvider implements vscode.HoverProvider {
       if (item.name === word) {
         markdownString.appendMarkdown(`### ${item.name}\n`);
         markdownString.appendMarkdown(`${item.description}\n`);
-        markdownString.appendMarkdown(`\n --- \n Option List\n\n`);
+        if (item.body.length) {
+          markdownString.appendMarkdown(`\n --- \n Option List\n\n`);
+        }
         item.body.forEach((content) => {
           markdownString.appendMarkdown(`- ${content.attr_name} : ${content.attr_desc}\n`);
 
