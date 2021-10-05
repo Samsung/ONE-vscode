bd0eb5e773dbba2ee9e390f3bfcfb8921733483f
yesfordev <yesfordev@gmail.com>
2021-10-05 11:57:18 +0900

    [Circletracer] Add a js file to visualize details of circle file
    This visualizes the details of the circle file.
    
    ONE-vscode-DCO-1.0-Signed-off-by: Youngeun Seo <yesfordev@gmail.com>

---
+++ media/Circletracer/makedata.js
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2021 Samsung Electronics Co., Ltd. All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+function openDetail() {
+  document.querySelector('#main').style.marginRight = '35%';
+  document.querySelector('#detail').style.width = '35%';
+  document.querySelector('#detail').style.display = 'block';
+}
+
+function closeDetail() {
+    document.querySelector('#main').style.marginRight = '0%';
+    document.querySelector('#detail').style.display = 'none';
+}
+
+function createDetailContent(nodes, id, g) {
+  var _node = g.node(id);
+
+  removeElementsByClass('detail-content-list');
+  nodes.forEach(node => {
+    if (node.index === id) {
+      for (let key in node) {
+        if (key === 'type' || key === 'location') {
+          createDetailItem(key, node[key], '#node-properties-content');
+        }
+
+        if (key === 'attributes') {
+          node[key].forEach(element => {
+            createDetailItem(element['attribute'], element['value'], '#attributes-content');
+          });
+        }
+
+        if (key === 'inputs') {
+          node[key].forEach((input, idx) => {
+            createDetailItem(`input ${idx}`, `name: ${input['name']}`, '#inputs-content');
+            createDetailItem('', `type: [${getTypeArray(',', input['type'])}]`, '#inputs-content');
+            createDetailItem('', `location: ${input['location']}`, '#inputs-content');
+          });
+        }
+
+        if (key === 'outputs') {
+          node[key].forEach((output, idx) => {
+            createDetailItem(`output ${idx}`, `name: ${output['name']}`, '#outputs-content');
+            createDetailItem(
+                '', `type: [${getTypeArray(',', output['type'])}]`, '#outputs-content');
+            createDetailItem('', `location: ${output['location']}`, '#outputs-content');
+          });
+        }
+      }
+    }
+  });
+  openDetail();
+}
