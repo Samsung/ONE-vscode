1e7c99b69230334c59d8e4f58e86c4549a101c84
donggillee-dev <donggillee94@gmail.com>
2021-10-05 23:09:38 +0900

    Add singleton.js

---
+++ media/Circletracer/singleton.js
@@ -0,0 +1,21 @@
+let singleton = (function() {
+  let instance;
+
+  function setter(json){
+    return {
+      durCircleJson: json
+    };
+  };
+
+  return {
+    setInstance: function (json) {
+      instance = setter(json);
+    },
+    getInstance: function () {
+      if (!instance) {
+        instance = foo(json);
+      }
+      return instance;
+    }
+  };
+})();
