1faa6b79e4b89cd39929eb0b0b000dd274f3b077
Yongseop Kim <dragon.seop@gmail.com>
2022-04-26 22:22:29 +0900

    [Project] Introduce JobCommand class (#509)
    Let's introduce JobCommand class.
    
    ONE-vscode-DCO-1.0-Signed-off-by: Yongseop Kim <<yons.kim@samsung.com>>

---
+++ src/Project/JobCommand.ts
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2022 Samsung Electronics Co., Ltd. All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import assert from 'assert';
+
+import {Command} from '../Backend/Command';
+
+import {Job} from './Job';
+import {ToolArgs} from './ToolArgs';
+
+// NOTE: JobBase will be replaced by this
+class JobCommand implements Job {
+  jobType: Job.Type;
+  name: string;
+  valid: boolean;
+  tool: string;
+  toolArgs: ToolArgs;
+  successCallback?: () => void;
+  failureCallback?: () => void;
+
+  constructor(cmd: Command) {
+    // should be implemented by child classes
+    this.jobType = Job.Type.tUndefined;
+    this.name = '';
+    this.valid = false;
+
+    // init by cmd
+    assert(cmd.length > 0);
+    this.tool = cmd[0];
+    this.toolArgs = new ToolArgs();
+    for (let i = 1; i < cmd.length; i++) {
+      this.toolArgs.push(cmd[i]);
+    }
+  }
+}
+
+export {JobCommand};
