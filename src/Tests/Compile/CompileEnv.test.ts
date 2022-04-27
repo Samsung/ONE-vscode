4201e812e77434dc2f7cefe7e16d0a00af7e1fce
Yongseop Kim <dragon.seop@gmail.com>
2022-04-27 12:25:43 +0900

    [Compile] Introduce CompileEnv class
    This commit introduces CompileEnv class.
    
    ONE-vscode-DCO-1.0-Signed-off-by: Yongseop Kim <<yons.kim@samsung.com>>

---
+++ src/Tests/Compile/CompileEnv.test.ts
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2022 Samsung Electronics Co., Ltd. All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {assert} from 'chai';
+
+import {Command} from '../../Backend/Command';
+import {CompileEnv} from '../../Compile/CompileEnv';
+
+suite('Compile', function() {
+  suite('CompileEnv', function() {
+    suite('#confirmInstalled()', function() {
+      test('', function() {
+
+      });
+    });
+
+    suite('#listAvailable()', function() {
+      test('', function() {
+
+      });
+    });
+
+    suite('#listInstalled()', function() {
+      test('', function() {
+
+      });
+    });
+
+    suite('#install()', function() {
+      test('', function() {
+
+      });
+    });
+
+    suite('#uninstall()', function() {
+      test('', function() {
+
+      });
+    });
+
+    suite('#compile()', function() {
+      test('', function() {
+
+      });
+    });
+  });
+});
