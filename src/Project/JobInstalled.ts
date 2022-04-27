425009276681cafe8cca81711dd396beee6fdd2f
Yongseop Kim <dragon.seop@gmail.com>
2022-04-27 09:28:16 +0900

    [Project] Add new Job classes
    This commit adds new Job classes - JobConfig, JobInstall, JobInstalled
    and JobUninstall.
    
    ONE-vscode-DCO-1.0-Signed-off-by: Yongseop Kim <<yons.kim@samsung.com>>

---
+++ src/Project/JobInstalled.ts
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2022 Samsung Electronics Co., Ltd. All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {Command} from '../Backend/Command';
+import {Job} from './Job';
+import {JobCommand} from './JobCommand';
+
+class JobInstalled extends JobCommand {
+  jobType: Job.Type;
+  name: string;
+  valid: boolean;
+
+  constructor(cmd: Command) {
+    super(cmd);
+    this.jobType = Job.Type.tInstalled;
+    this.name = 'installed';
+    this.valid = true;
+  }
+}
+
+export {JobInstalled};
---
+++ src/Tests/Project/JobInstall.test.ts
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2022 Samsung Electronics Co., Ltd. All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {assert} from 'chai';
+
+import {Command} from '../../Backend/Command';
+import {Job} from '../../Project/Job';
+import {JobInstall} from '../../Project/JobInstall';
+
+suite('Project', function() {
+  suite('JobInstall', function() {
+    suite('#contructor()', function() {
+      test('is contructed with command', function() {
+        let cmd = new Command('apt-get');
+        cmd.push('install');
+        cmd.push('pkgname');
+
+        let job = new JobInstall(cmd);
+        assert.deepStrictEqual(job.tool, cmd[0]);
+        assert.deepStrictEqual(job.toolArgs[0], cmd[1]);
+        assert.deepStrictEqual(job.toolArgs[1], cmd[2]);
+        assert.deepStrictEqual(job.name, 'install');
+        assert.equal(job.jobType, Job.Type.tInstall);
+        assert.isTrue(job.valid);
+      });
+    });
+  });
+});
