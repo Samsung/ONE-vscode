cea004f0e13d171a1d1774c5e7703017fcb4c1eb
Dayoung Lee <dayg502@gmail.com>
2022-06-08 16:39:40 +0900

    [CfgEditor] Rename command names (#759)
    * [CfgEditor] Rename command names
    
    This commit renames command names from
    onevscode.* to one.explorer.* and so on.
    
    Related to #728
    
    ONE-vscode-DCO-1.0-Signed-off-by: dayo09 <dayoung.lee@samsung.com>
    
    * Rename configuration-settings and so on
    
    ONE-vscode-DCO-1.0-Signed-off-by: dayo09 <dayoung.lee@samsung.com>
    
    Co-authored-by: Dayoung Lee <dayoung.lee@samsung.com>

--- src/extension.ts
+++ src/extension.ts
@@ -89,7 +89,7 @@ export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(disposableOneJsontracer);
 
   let disposableOneConfigurationSettings =
-      vscode.commands.registerCommand('onevscode.configuration-settings', () => {
+      vscode.commands.registerCommand('one.editor.openCfgWithLegacyEditor', () => {
         ConfigPanel.createOrShow(context);
         Logger.info(tag, 'one configuration settings...');
       });
@@ -98,7 +98,7 @@ export function activate(context: vscode.ExtensionContext) {
   createStatusBarItem(context);
 
   let disposableToggleCodelens =
-      vscode.commands.registerCommand('onevscode.toggle-codelens', () => {
+      vscode.commands.registerCommand('one.editor.toggleCodelens', () => {
         let codelensState =
             vscode.workspace.getConfiguration('one-vscode').get('enableCodelens', true);
         vscode.workspace.getConfiguration('one-vscode')
