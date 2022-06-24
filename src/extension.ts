05a07ff0578d827ff96687ee610668f7fcc6854e
SaeHie Park <saehie.park@gmail.com>
2022-06-24 18:27:52 +0900

    [CircleGraph] Remove old graph viewer
    This will remove old graph viewer with command palette.
    
    ONE-vscode-DCO-1.0-Signed-off-by: SaeHie Park <saehie.park@gmail.com>

--- src/extension.ts
+++ src/extension.ts
@@ -18,7 +18,6 @@ import * as vscode from 'vscode';
 
 import {backendRegistrationApi} from './Backend/Backend';
 import {CfgEditorPanel} from './CfgEditor/CfgEditorPanel';
-import {CircleGraphPanel} from './CircleGraph/CircleGraph';
 import {CircleViewerProvider} from './CircleGraph/CircleViewer';
 import {decoder} from './Circlereader/Circlereader';
 import {Circletracer} from './Circletracer';
@@ -137,11 +136,6 @@ export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(disposableOneCircleTracer);
 
   context.subscriptions.push(MondrianEditorProvider.register(context));
-
-  let disposableGraphPenel = vscode.commands.registerCommand('one.viewer.circleGraph', () => {
-    CircleGraphPanel.createOrShow(context.extensionUri, undefined);
-  });
-  context.subscriptions.push(disposableGraphPenel);
 
   context.subscriptions.push(PartEditorProvider.register(context));
   context.subscriptions.push(PartGraphSelPanel.register(context));
