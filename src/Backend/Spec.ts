736a3e3788233ce6344045b8c4807b031c508006
struss <rrstrous@nate.com>
2022-07-04 16:23:57 +0900

    [DeviceAPI] Add Spec on Backend (#837)
    * [DeviceAPI] Add Spec on Backend
    
    This commit will add DeviceAPI which will use on Device specify information.
    
    ONE-vscode-DCO-1.0-Signed-off-by: struss <rrstrous@nate.com>
    
    * Fix format.
    
    * Fix commented.
    
    * Add test about `Spec.ts`
    
    * Fix commented.
    
    * Fix commented and update test.
    
    * remove unused libararies.
    
    * Fix test spec little bit.
    
    * Update src/Backend/Spec.ts
    
    Co-authored-by: Hyun Sik Yoon <63768207+hyunsik-yoon@users.noreply.github.com>
    
    * Update src/Backend/Spec.ts
    
    Co-authored-by: Hyun Sik Yoon <63768207+hyunsik-yoon@users.noreply.github.com>
    
    * modify comment about `BridgeSpec`
    
    * Remove bash from test code.
    
    Co-authored-by: Hyun Sik Yoon <63768207+hyunsik-yoon@users.noreply.github.com>

---
+++ src/Backend/Spec.ts
@@ -0,0 +1,92 @@
+/*
+ * Copyright (c) 2022 Samsung Electronics Co., Ltd. All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {Command} from './Command';
+
+/**
+ * Spec is a bundle of information that could be used for checking whether something to require is
+ * satisfied with a certain objective.
+ */
+
+// TODO: Split DeviceSpec and BridgeSpec.
+
+/**
+ * DeviceSpec is a Spec to specify a device.
+ * Each instance for each device should have a matching DeviceSpec.
+ */
+class DeviceSpec {
+  // TODO: make this properties more specific or new prorperty can be added.
+  // Currently `hw` comes from 'uname -m', but this could be updated
+  hw: string;
+  // Currently `sw` comes from 'cat /etc/os-release' with $(NAME), but this could be updated
+  sw: string;
+  bridge: BridgeSpec|undefined;  // this will be used to access on this spec.
+
+  constructor(hw: string, sw: string, bridge: BridgeSpec|undefined) {
+    this.hw = hw;
+    this.sw = sw;
+    this.bridge = bridge;
+  }
+
+  // This will be used on check Executor Spec could cover.
+  satisfied(spec: DeviceSpec): boolean {
+    // TODO Specify how to check spec satisfied.
+    if (spec.hw.includes(this.hw) && spec.sw.includes(this.sw)) {
+      return true;
+    }
+    return false;
+  }
+  // TODO Add more Oprator to check it could cover some other spec.
+}
+
+/**
+ * BridgeSpec is a Spec that is used for access on a device.
+ */
+class BridgeSpec {
+  name: string;
+  // This will return Command to get TargetDevice List.
+  deviceListCmd: Command;
+  // This will return Command to run on TargetDevice.
+  shellCmd: Command;
+  // TODO add more command for this.
+  constructor(name: string, listCommand: string, shellCommand: string) {
+    this.name = name;
+    this.deviceListCmd = new Command(listCommand);
+    this.shellCmd = new Command(shellCommand);
+  }
+}
+
+// TODO add more BridgeSpec like docker or ADB......
+const sdbSpec = new BridgeSpec(
+    'sdb', 'sdb devices | grep -v devices | grep device | awk \'{print $1}\'', 'sdb shell');
+
+class HostPCSpec extends DeviceSpec {
+  constructor(hw: string, sw: string) {
+    super(hw, sw, undefined);
+  }
+}
+
+class TizenDeviceSpec extends DeviceSpec {
+  constructor(hw: string, sw: string) {
+    super(hw, sw, sdbSpec);
+  }
+}
+
+const supportedSpecs = new Array<DeviceSpec>(
+    new HostPCSpec('x86_64', 'Ubuntu 18') /* spec where simulator can run */,
+    new TizenDeviceSpec('armv7l', 'Tizen 7.0.0') /* spec for Tizen TV */);
+
+export {DeviceSpec, BridgeSpec, HostPCSpec, TizenDeviceSpec, sdbSpec, supportedSpecs};
---
+++ src/Tests/Backend/Spec.test.ts
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2022 Samsung Electronics Co., Ltd. All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {assert, should} from 'chai';
+
+import {BridgeSpec, DeviceSpec, HostPCSpec, sdbSpec, TizenDeviceSpec} from '../../Backend/Spec';
+
+suite('Spec', function() {
+  suite('DeviceSpec', function() {
+    suite('#contructor()', function() {
+      const hw: string = 'TestHW';
+      const sw: string = 'TestOS';
+      const testSpec = new DeviceSpec(hw, sw, undefined);
+      assert.isObject<DeviceSpec>(testSpec);
+      assert.strictEqual(testSpec.hw, hw);
+      assert.strictEqual(testSpec.sw, sw);
+      assert.strictEqual(testSpec.bridge, undefined);
+    });
+    suite('#satisfied()', function() {
+      const executorSpec = new DeviceSpec('arm', 'Tizen', undefined);
+      const dummySpec1 = new TizenDeviceSpec('armv7l', 'Tizen 7.0.0 (Tizen7/TV)');
+      const dummySpec2 = new HostPCSpec('x86_64', 'Ubuntu 20.04.4 LTS');
+      assert.isTrue(executorSpec.satisfied(dummySpec1));
+      assert.isFalse(executorSpec.satisfied(dummySpec2));
+    });
+  });
+  suite('TizenDeviceSpec', function() {
+    suite('#contructor()', function() {
+      const hw: string = 'armv7l';
+      const sw: string = 'Tizen 7.0.0';
+      const testSpec = new TizenDeviceSpec(hw, sw);
+      assert.isObject<TizenDeviceSpec>(testSpec);
+      assert.strictEqual(testSpec.hw, hw);
+      assert.strictEqual(testSpec.sw, sw);
+      assert.strictEqual(testSpec.bridge, sdbSpec);
+    });
+  });
+  suite('HostPCSpec', function() {
+    suite('#contructor()', function() {
+      const hw: string = 'x86_64';
+      const sw: string = 'Ubuntu 18';
+      const testSpec = new HostPCSpec(hw, sw);
+      assert.isObject<HostPCSpec>(testSpec);
+      assert.strictEqual(testSpec.hw, hw);
+      assert.strictEqual(testSpec.sw, sw);
+      assert.strictEqual(testSpec.bridge, undefined);
+    });
+  });
+  suite('BridgeSpec', function() {
+    suite('#constructor()', function() {
+      const dummyDeviceList = 'testDeiveList';
+      const dummyShell = 'testBridgeShell';
+      const testBridgeSpec = new BridgeSpec('test', dummyDeviceList, dummyShell);
+      assert.isObject<BridgeSpec>(testBridgeSpec);
+      assert.strictEqual(testBridgeSpec.name, 'test');
+      assert.strictEqual(testBridgeSpec.deviceListCmd.str(), dummyDeviceList);
+      assert.strictEqual(testBridgeSpec.shellCmd.str(), dummyShell);
+    });
+  });
+  suite('sdbSpec', function() {
+    assert.isObject<BridgeSpec>(sdbSpec);
+    assert.strictEqual(sdbSpec.name, 'sdb');
+    assert.strictEqual(
+        sdbSpec.deviceListCmd.str(),
+        'sdb devices | grep -v devices | grep device | awk \'{print $1}\'');
+    assert.strictEqual(sdbSpec.shellCmd.str(), 'sdb shell');
+  });
+});
