23ac1db3a2bbd2089757b4cfd1d01147ea3957f3
Yongseop Kim <dragon.seop@gmail.com>
2022-07-07 00:04:52 +0900

    Prepare tests
    ONE-vscode-DCO-1.0-Signed-off-by: Yongseop Kim <<yons.kim@samsung.com>>

---
+++ src/Tests/Execute/InferenceQuickInput.test.ts
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2022 Samsung Electronics Co., Ltd. All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {assert} from 'chai';
+
+import {InferenceQuickInput} from '../../Execute/InferenceQuickInput';
+
+suite('Execute', function() {
+  suite('InferenceQuickInput', function() {
+    suite('#constructor()', function() {
+      test('', function() {
+
+      });
+    });
+
+    suite('#getBackend()', function() {
+      test('', function() {
+
+      });
+    });
+
+    suite('#getModelPath()', function() {
+      test('', function() {
+
+      });
+    });
+
+    suite('#getInputSpec()', function() {
+      test('', function() {
+
+      });
+    });
+
+    suite('#getError()', function() {
+      test('', function() {
+
+      });
+    });
+
+    suite('#getAllBackendNames()', function() {
+      test('', function() {
+
+      });
+    });
+
+    suite('#getQuickPickItems()', function() {
+      test('', function() {
+
+      });
+    });
+
+    suite('#getBackendFromGlobal()', function() {
+      test('', function() {
+
+      });
+    });
+
+    suite('#getFilter()', function() {
+      test('', function() {
+
+      });
+    });
+
+    suite('#getInputSpecKeys()', function() {
+      test('', function() {
+
+      });
+    });
+  });
+});
