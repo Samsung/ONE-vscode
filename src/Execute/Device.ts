39f99af8f1fdd72bdac4cef5f7d9a5665c9e27fe
struss <rrstrous@nate.com>
2022-07-08 13:29:52 +0900

    [DeviceAPI] Add Device API on Execute (#935)
    * [DeviceAPI] Add Device API on Execute
    
    This commit will add `Device` API on Execute
    
    ONE-vscode-DCO-1.0-Signed-off-by: struss <rrstrous@nate.com>
    
    * Fix commented.
    
    * Change class name from `DeviceInstance` to `Device`
    
    * Update src/Execute/Device.ts
    
    Co-authored-by: Hyun Sik Yoon <63768207+hyunsik-yoon@users.noreply.github.com>
    
    Co-authored-by: Hyun Sik Yoon <63768207+hyunsik-yoon@users.noreply.github.com>

---
+++ src/Execute/Device.ts
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2022 Samsung Electronics Co., Ltd. All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {DeviceSpec} from '../Backend/Spec';
+
+/**
+ * `Device` class
+ * a class to handle and access Device.
+ * Each Device has single `DeviceSpec`, which could be same with other device.
+ * Each Device has a name to specify with other device with same `DeviceSpec`.
+ *
+ * for example, Device A, B, C connected on, and A and B are TizenTV and C is HostPC.
+ * then we could access this as below
+ *
+ * const deviceA = new Device("A", TizenDeviceSpec);
+ * const deviceB = new Device("B", TizenDeviceSpec);
+ * const deviceC = new Device("C", HostPCSpec);
+ */
+class Device {
+  name: string;
+  spec: DeviceSpec;
+  constructor(name: string, spec: DeviceSpec) {
+    this.name = name;
+    this.spec = spec;
+  }
+};
+
+export {Device};
---
+++ src/Tests/Execute/Device.test.ts
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2022 Samsung Electronics Co., Ltd. All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {assert} from 'chai';
+import {DeviceSpec, HostPCSpec, TizenDeviceSpec} from '../../Backend/Spec';
+
+import {Device} from '../../Execute/Device';
+
+suite('Device', function() {
+  suite('Device', function() {
+    suite('#contructor()', function() {
+      const deviceName: string = 'testDevice';
+      const hw: string = 'TestHW';
+      const sw: string = 'TestOS';
+      const testSpec = new DeviceSpec(hw, sw, undefined);
+      const testDevice = new Device(deviceName, testSpec);
+      assert.isObject<Device>(testDevice);
+      assert.strictEqual(testDevice.spec, testSpec);
+      assert.strictEqual(testDevice.name, deviceName);
+    });
+  });
+
+  suite('TizenTVDevice', function() {
+    suite('#contructor()', function() {
+      const deviceName: string = '127.0.0.1:26101';
+      const hw: string = 'armv7l';
+      const sw: string = 'Tizen 7.0.0';
+      const testSpec = new TizenDeviceSpec(hw, sw);
+      const tizenTV = new Device(deviceName, testSpec);
+      assert.isObject<Device>(tizenTV);
+      assert.strictEqual(tizenTV.spec, testSpec);
+      assert.strictEqual(tizenTV.name, deviceName);
+    });
+  });
+
+  suite('HostPCDevice', function() {
+    suite('#constructor()', function() {
+      const deviceName: string = 'hostPC-test-DeskTop';
+      const hw: string = 'x86_64';
+      const sw: string = 'Ubuntu 18';
+      const testSpec = new HostPCSpec(hw, sw);
+      const hostPC = new Device(deviceName, testSpec);
+      assert.isObject<Device>(hostPC);
+      assert.strictEqual(hostPC.spec, testSpec);
+      assert.strictEqual(hostPC.name, deviceName);
+    });
+  });
+});
