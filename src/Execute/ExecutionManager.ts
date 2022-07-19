e5abc4dab4b650863879ebb8a9e6147a28c4be21
struss <rrstrous@nate.com>
2022-07-19 18:40:29 +0900

    Fix format.

--- src/Execute/ExecutionManager.ts
+++ src/Execute/ExecutionManager.ts
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { Executor } from '../Backend/Executor';
+import {Executor} from '../Backend/Executor';
 import {Device} from './Device';
 
 class ExecutionManager {
