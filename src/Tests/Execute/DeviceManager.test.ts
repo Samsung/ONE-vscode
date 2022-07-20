b06c59e975ca95aa750ac84a7d83a5d188c935a0
struss <rrstrous@nate.com>
2022-07-20 19:55:37 +0900

    [DeviceAPI] Add ExecutionManager (#1045)
    * [DeviceAPI] Add ExecutionManager
    
    This commit will add `ExecuteManager` to handle `Device` and 'Executor'.
    
    ONE-vscode-DCO-1.0-Signed-off-by: struss <rrstrous@nate.com>
    
    * Move available Set to Device.
    
    * Update src/Execute/ExecutionManager.ts
    
    Co-authored-by: Dayoung Lee <dayg502@gmail.com>
    
    * Fix format.
    
    * Fix name of method.
    
    * Add test for ExecutionManager.
    
    * Fix format.
    
    * rename `ExectionManager` to `DeviceManager`.
    
    * Fix test code.
    
    Co-authored-by: Dayoung Lee <dayg502@gmail.com>

---
+++ src/Tests/Execute/DeviceManager.test.ts
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2022 Samsung Electronics Co., Ltd. All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {assert} from 'chai';
+import {Executor, ExecutorBase} from '../../Backend/Executor';
+import {DeviceSpec} from '../../Backend/Spec';
+import {Device} from '../../Execute/Device';
+import {DeviceManager} from '../../Execute/DeviceManager';
+
+const dummyDeviceList: Device[] = [
+  new Device('dummyDevice1', new DeviceSpec('dummySpecHW1', 'dummySpecSW1', undefined)),
+  new Device('dummyDevice2', new DeviceSpec('dummySpecHW1', 'dummySpecSW2', undefined)),
+  new Device('dummyDevice3', new DeviceSpec('dummySpecHW2', 'dummySpecSW2', undefined))
+];
+
+class DummyExecutor1 extends ExecutorBase {
+  name(): string {
+    return 'dummyExecutor1';
+  }
+  require(): DeviceSpec {
+    return new DeviceSpec('dummySpecHW1', 'dummySpecSW1', undefined);
+  }
+}
+
+class DummyExecutor2 extends ExecutorBase {
+  name(): string {
+    return 'dummyExecutor2';
+  }
+  require(): DeviceSpec {
+    return new DeviceSpec('dummySpecHW2', 'dummySpecSW2', undefined);
+  }
+}
+
+const dummyExecutorList: Executor[] = [new DummyExecutor1(), new DummyExecutor2()];
+
+suite('DeviceManager', function() {
+  suite('DeviceManager', function() {
+    suite('#construtor()', function() {
+      const deviceManager = new DeviceManager(dummyDeviceList, dummyExecutorList);
+      assert.isObject<DeviceManager>(deviceManager);
+      assert.strictEqual(dummyDeviceList, deviceManager.allDevices);
+      assert.isTrue(deviceManager.allDevices[0].availableExecutors.has(dummyExecutorList[0]));
+      assert.isTrue(deviceManager.allDevices[2].availableExecutors.has(dummyExecutorList[1]));
+    });
+    suite('#findDevice()', function() {
+      const deviceManager = new DeviceManager(dummyDeviceList, dummyExecutorList);
+      assert.strictEqual(deviceManager.findDevice('dummyDevice1'), dummyDeviceList[0]);
+      assert.strictEqual(deviceManager.findDevice('dummyDevice2'), dummyDeviceList[1]);
+      assert.strictEqual(deviceManager.findDevice('dummyDevice3'), dummyDeviceList[2]);
+    });
+  });
+});
