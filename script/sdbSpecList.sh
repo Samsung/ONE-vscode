dc4fac352d62c5452804c309288b3bda514fc2c3
struss <rrstrous@nate.com>
2022-07-22 16:20:33 +0900

    Fix comment more correctly.

--- script/sdbSpecList.sh
+++ script/sdbSpecList.sh
@@ -3,18 +3,20 @@
 #
 # This shell script prints tizen device serial name on each line.
 #
-# on `sdb devices`, it will show result below.
+# On `sdb devices`, it will show result below.
 #
 # List of devices attached 
 # 192.168.223.1:26101 	device    	0
+# emulator-1            emulator    0
 # 192.168.224.1:26101 	device    	0
 #
-# so, only imformation we need is list of -s in first column from second row to end.
-# just pass `sdb device` result except first line and grep only device and pring first column
-# this will show result below
+# First colmn is serialNumber which is unique key for device.
+# Second colmn is state.
+# Third colmn is targetName.
+# (Reference: https://docs.tizen.org/application/tizen-studio/common-tools/smart-development-bridge/#managing-targets)
 #
-# 192.168.223.1:26101
-# 192.168.224.1:26101
+# `sdb devices` will not give data that exactly we want, So we need to modify result.
+# We only need serialNumber, So use `awk` to pick serialNumber information.
 #
 
-sdb devices | awk 'NR > 1 {print}' | grep device | awk '{print $1}'
+sdb devices | awk 'NR > 1 {if ($2 == "device") print $1}'
