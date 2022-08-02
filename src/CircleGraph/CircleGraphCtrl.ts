7178072f16fecd4ce90fb9a5cfa49327ca342841
Seok Namkoong <seok9311@naver.com>
2022-08-02 17:32:08 +0900

    [CircleGraph] Use external source code instead of modified source code (#1125)
    `media/CircleGraph/view-sidebar.js` has following modifications.
    - Change formatting
    - Change `==` to `===`
    - Change `!=` to `!==`
    However, these modifications are not related with real operation.
    Thus this commit will replace `media/CircleGraph/view-sidebar.js` to `media/CircleGraph/external/view-sidebar.js`.
    
    ONE-vscode-DCO-1.0-Signed-off-by: Seok NamKoong <seok9311@naver.com>

--- src/CircleGraph/CircleGraphCtrl.ts
+++ src/CircleGraph/CircleGraphCtrl.ts
@@ -368,7 +368,7 @@ export class CircleGraphCtrl {
     // necessary files from netron to work
     html = this.updateUri(html, webview, '%view-grapher.css%', 'view-grapher.css');
     html = this.updateUri(html, webview, '%view-sidebar.css%', 'view-sidebar.css');
-    html = this.updateUri(html, webview, '%view-sidebar.js%', 'view-sidebar.js');
+    html = this.updateExternalUri(html, webview, '%view-sidebar.js%', 'view-sidebar.js');
     html = this.updateUri(html, webview, '%view-grapher.js%', 'view-grapher.js');
     html = this.updateExternalUri(html, webview, '%dagre.js%', 'dagre.js');
     html = this.updateExternalUri(html, webview, '%base.js%', 'base.js');
