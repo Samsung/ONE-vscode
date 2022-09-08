cd666339f6a55fa8bb819aeb02a5dee31b449f8f
Dayoung Lee <dayoung.lee@samsung.com>
2022-09-08 19:42:26 +0900

    [OneExplorer] Reveal in OneExplorer when cfg is focused
    ONE-vscode-DCO-1.0-Signed-off-by: Dayoung Lee <dayoung.lee@samsung.com>

--- src/OneExplorer/OneExplorer.ts
+++ src/OneExplorer/OneExplorer.ts
@@ -468,7 +468,9 @@ export class OneTreeDataProvider implements vscode.TreeDataProvider<Node> {
 
   private fileWatcher = vscode.workspace.createFileSystemWatcher(`**/*`);
 
-  private tree: DirectoryNode|undefined;
+  private _tree: DirectoryNode|undefined;
+  private _nodeMap: Map<string, Node> = new Map<string, Node>();
+
   public static didHideExtra: boolean = false;
 
   public static register(context: vscode.ExtensionContext) {
@@ -493,13 +495,23 @@ export class OneTreeDataProvider implements vscode.TreeDataProvider<Node> {
 
     const provider = new OneTreeDataProvider(workspaceRoot, context.extension.extensionKind);
 
+    const _treeView = vscode.window.createTreeView(
+        'OneExplorerView',
+        {treeDataProvider: provider, showCollapseAll: true, canSelectMany: true});
+
     let registrations = [
       provider.fileWatcher.onDidCreate(() => provider.refresh()),
       provider.fileWatcher.onDidChange(() => provider.refresh()),
       provider.fileWatcher.onDidDelete(() => provider.refresh()),
-      vscode.window.createTreeView(
-          'OneExplorerView',
-          {treeDataProvider: provider, showCollapseAll: true, canSelectMany: true}),
+      _treeView,
+      vscode.commands.registerCommand(
+          'one.explorer.revealInOneExplorer',
+          (path: string) => {
+            const node = provider._nodeMap.get(path);
+            if (node) {
+              _treeView ?.reveal(node, {select: true, focus: true, expand: true});
+            }
+          }),
       vscode.commands.registerCommand(
           'one.explorer.openAsText',
           (node: Node) => {
@@ -602,7 +614,8 @@ export class OneTreeDataProvider implements vscode.TreeDataProvider<Node> {
 
     if (!node) {
       // Reset the root in order to build from scratch (at OneTreeDataProvider.getTree)
-      this.tree = undefined;
+      this._tree = undefined;
+      this._nodeMap.clear();
       this._onDidChangeTreeData.fire(undefined);
     } else {
       this._onDidChangeTreeData.fire(node);
@@ -772,11 +785,20 @@ input_path=${modelName}.${extName}
       return undefined;
     }
 
-    if (!this.tree) {
-      this.tree = NodeFactory.create(NodeType.directory, this.workspaceRoot.fsPath, undefined) as
+    if (!this._tree) {
+      this._tree = NodeFactory.create(NodeType.directory, this.workspaceRoot.fsPath, undefined) as
           DirectoryNode;
+
+      const buildNodeMap = (node: Node) => {
+        node.getChildren().forEach(childNode => {
+          this._nodeMap.set(childNode.path, childNode);
+          buildNodeMap(childNode);
+        });
+      };
+
+      buildNodeMap(this._tree);
     }
 
-    return this.tree;
+    return this._tree;
   }
 }
