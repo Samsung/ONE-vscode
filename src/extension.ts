e0364d7f114425bf32d8a2b35b47758ca88b6eac
hm <67681207+gandi0330@users.noreply.github.com>
2022-09-21 16:37:22 +0900

    [Metadata Manager] rename function (#35)
    This commit is to change several variables and function names
    
    Co-authored-by: Ubuntu <hm@hm.4t4hclpmegouxmcq0lkdmmdanc.syx.internal.cloudapp.net>

--- src/extension.ts
+++ src/extension.ts
@@ -26,7 +26,7 @@ import {OneTreeDataProvider} from './OneExplorer/OneExplorer';
 import {PartEditorProvider} from './PartEditor/PartEditor';
 import {PartGraphSelPanel} from './PartEditor/PartGraphSelector';
 import { ToolchainProvider } from './Toolchain/ToolchainProvider';
-import { Metadata } from './Metaprovider/metadata_manager';
+import { Metadata } from './MetadataManager/metadataAPI';
 import {Logger} from './Utils/Logger';
 
 /* istanbul ignore next */
