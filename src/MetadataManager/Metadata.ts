55f4199a77d2b35eef8bab3e4addcc3b691e92dc
Changho Shin <chshin59@gmail.com>
2022-10-02 17:25:36 +0900

    This commit refactors function and variable name.

--- src/MetadataManager/Metadata.ts
+++ src/MetadataManager/Metadata.ts
@@ -20,7 +20,7 @@ import * as vscode from 'vscode';
 import {ToolchainInfo} from '../Backend/Toolchain';
 import {obtainWorkspaceRoot} from '../Utils/Helpers';
 
-import {getStats, isValidFile} from './Utils';
+import {isOneExplorerTargetFile} from '../Utils/Helpers';
 
 type BuildInfoKeys = 'onecc'|'toolchain'|'cfg';
 
@@ -31,19 +31,6 @@ interface BuildInfoObj {
 export class BuildInfo {
   private static _map = new Map<string, BuildInfoObj>();
 
-  public static get(metadata: any, uri: vscode.Uri) {
-    const path = vscode.workspace.asRelativePath(uri);
-    const info = BuildInfo._map.get(path);
-    if (info) {
-      metadata['onecc-version'] = info['onecc'];
-      metadata['toolchain-version'] = info['toolchain'] ?.version ?.str();
-      metadata['cfg-settings'] = info['cfg'];
-    }
-
-    BuildInfo._map.delete(path);
-    return info;
-  }
-
   public static set(path: string, key: BuildInfoKeys, value: any) {
     const relPath = vscode.workspace.asRelativePath(path);
     let info = BuildInfo._map.get(relPath);
@@ -52,6 +39,17 @@ export class BuildInfo {
       BuildInfo._map.set(relPath, info);
     }
     info[key] = value;
+  }
+
+  public static save(metaEntry: any, uri: vscode.Uri) {
+    const path = vscode.workspace.asRelativePath(uri);
+    const info = BuildInfo._map.get(path);
+    if (info) {
+      metaEntry['onecc-version'] = info['onecc'];
+      metaEntry['toolchain-version'] = info['toolchain'] ?.version ?.str();
+      metaEntry['cfg-settings'] = info['cfg'];
+    }
+    BuildInfo._map.delete(path);
   }
 }
 
@@ -83,10 +81,10 @@ export class Metadata {
   }
 
   public static async getEntry(uri: vscode.Uri, hash: string) {
-    const metadata = await Metadata.getObj(hash);
+    const metaObj = await Metadata.getObj(hash);
     const relPath = vscode.workspace.asRelativePath(uri);
-    if (metadata && metadata[relPath] && Object.keys(metadata[relPath]).length !== 0) {
-      return metadata[relPath];
+    if (metaObj && metaObj[relPath] && Object.keys(metaObj[relPath]).length !== 0) {
+      return metaObj[relPath];
     }
     // TODO: Error Handling
     return undefined;
@@ -94,20 +92,20 @@ export class Metadata {
 
   public static async setEntry(uri: vscode.Uri, hash: string, entry: any) {
     const workspaceroot = obtainWorkspaceRoot();
-    const metadataUri = vscode.Uri.joinPath(
+    const jsonUri = vscode.Uri.joinPath(
         vscode.Uri.file(workspaceroot),
         `.meta/hash_objects/${hash.substring(0, 2)}/${hash.substring(2)}.json`);
 
     const relPath = vscode.workspace.asRelativePath(uri);
-    let metadata = await Metadata.getObj(hash);
-    if (!metadata[relPath]) {
-      metadata[relPath] = {};
+    const metaObj = await Metadata.getObj(hash);
+    if (!metaObj[relPath]) {
+      metaObj[relPath] = {};
     }
     for (let key in entry) {
-      metadata[relPath][key] = entry[key];
+      metaObj[relPath][key] = entry[key];
     }
     await vscode.workspace.fs.writeFile(
-        metadataUri, Buffer.from(JSON.stringify(metadata, null, 4), 'utf8'));
+        jsonUri, Buffer.from(JSON.stringify(metaObj, null, 4), 'utf8'));
   }
 
   public static async createDefault(uri: vscode.Uri, hash: string) {
@@ -115,55 +113,54 @@ export class Metadata {
       return;
     }
 
-    let metadata: any = await Metadata.getObj(hash);
+    let metaObj: any = await Metadata.getObj(hash);
     const relPath = vscode.workspace.asRelativePath(uri);
 
-    if (!metadata) {
-      metadata = {};
-      await Metadata.setObj(hash, metadata);
+    if (metaObj === undefined) {
+      metaObj = {};
+      await Metadata.setObj(hash, metaObj);
     }
-    if (!metadata[relPath]) {
-      metadata[relPath] = {};
+    if (metaObj[relPath] === undefined) {
+      metaObj[relPath] = {};
     }
 
     const filename: any = relPath.split('/').pop();
-    const stats: any =
-        await getStats(vscode.Uri.joinPath(vscode.workspace.workspaceFolders[0].uri, relPath));
+    const stats: any = fs.statSync(uri.fsPath);
 
-    metadata[relPath]['name'] = filename;
-    metadata[relPath]['file-extension'] = filename.split('.').pop();
-    metadata[relPath]['create-time'] = stats.birthtime;
-    metadata[relPath]['modified-time'] = stats.mtime;
-    metadata[relPath]['is-deleted'] = false;
-    await Metadata.setObj(hash, metadata);
+    metaObj[relPath]['name'] = filename;
+    metaObj[relPath]['file-extension'] = filename.split('.').pop();
+    metaObj[relPath]['create-time'] = stats.birthtime;
+    metaObj[relPath]['modified-time'] = stats.mtime;
+    metaObj[relPath]['is-deleted'] = false;
+    await Metadata.setObj(hash, metaObj);
   }
 
   // NOTE When the deleted file is recovered, metadata is also recovered.
   //    For that situation, metadata is not deleted but deactivated.
   public static async disable(uri: vscode.Uri, hash: string) {
-    if (!isValidFile(uri)) {
+    if (!isOneExplorerTargetFile(uri)) {
       return;
     }
     // step 1. Find hash object with hash value
-    const metadata = await Metadata.getObj(hash);
+    const metaObj = await Metadata.getObj(hash);
 
     // step 2. Check if the hash object has the deleted uri
     const relPath = vscode.workspace.asRelativePath(uri);
-    if (metadata === undefined || metadata[relPath] === undefined ||
-        Object.keys(metadata[relPath]).length !== 0) {
+    if (metaObj === undefined || metaObj[relPath] === undefined ||
+        Object.keys(metaObj[relPath]).length === 0) {
       return;
     }
 
     // step 3. deactivate (set 'is_deleted') that path.
-    metadata[relPath]['is-deleted'] = true;
-    await Metadata.setObj(hash, metadata);
+    metaObj[relPath]['is-deleted'] = true;
+    await Metadata.setObj(hash, metaObj);
   }
 
   public static async delete(uri: vscode.Uri, hash: string) {
-    const metadata = await Metadata.getObj(hash);
+    const metaObj = await Metadata.getObj(hash);
     const relPath = vscode.workspace.asRelativePath(uri);
-    delete metadata[relPath];
-    await Metadata.setObj(hash, metadata);
+    delete metaObj[relPath];
+    await Metadata.setObj(hash, metaObj);
   }
 
   // NOTE enable does not seem necessary because eventmanager does not call it.
