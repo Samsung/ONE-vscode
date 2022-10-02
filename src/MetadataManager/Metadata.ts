b66170f255a78b9c89a54a3f9a6834d7fec8578d
Changho Shin <chshin59@gmail.com>
2022-10-02 16:49:23 +0900

    This commit removes functions that do not fit metadata class.

--- src/MetadataManager/Metadata.ts
+++ src/MetadataManager/Metadata.ts
@@ -20,7 +20,6 @@ import * as vscode from 'vscode';
 import {ToolchainInfo} from '../Backend/Toolchain';
 import {obtainWorkspaceRoot} from '../Utils/Helpers';
 
-import {} from './PathToHash';
 import {getStats, isValidFile} from './Utils';
 
 type BuildInfoKeys = 'onecc'|'toolchain'|'cfg';
@@ -31,7 +30,7 @@ interface BuildInfoObj {
 
 export class BuildInfo {
   private static _map = new Map<string, BuildInfoObj>();
-  
+
   public static get(metadata: any, uri: vscode.Uri) {
     const path = vscode.workspace.asRelativePath(uri);
     const info = BuildInfo._map.get(path);
@@ -80,8 +79,7 @@ export class Metadata {
     const jsonUri = vscode.Uri.joinPath(
         vscode.Uri.file(workspaceroot),
         `.meta/hash_objects/${hash.substring(0, 2)}/${hash.substring(2)}.json`);
-    await vscode.workspace.fs.writeFile(
-      jsonUri, Buffer.from(JSON.stringify(obj, null, 4), 'utf8'));
+    await vscode.workspace.fs.writeFile(jsonUri, Buffer.from(JSON.stringify(obj, null, 4), 'utf8'));
   }
 
   public static async getEntry(uri: vscode.Uri, hash: string) {
@@ -94,25 +92,24 @@ export class Metadata {
     return undefined;
   }
 
-  public static async setEntry(uri: vscode.Uri, hash: string, obj: any) {
+  public static async setEntry(uri: vscode.Uri, hash: string, entry: any) {
     const workspaceroot = obtainWorkspaceRoot();
     const metadataUri = vscode.Uri.joinPath(
-      vscode.Uri.file(workspaceroot),
-      `.meta/hash_objects/${hash.substring(0, 2)}/${hash.substring(2)}.json`);
+        vscode.Uri.file(workspaceroot),
+        `.meta/hash_objects/${hash.substring(0, 2)}/${hash.substring(2)}.json`);
 
     const relPath = vscode.workspace.asRelativePath(uri);
     let metadata = await Metadata.getObj(hash);
     if (!metadata[relPath]) {
       metadata[relPath] = {};
     }
-    for (let key in obj) {
-      metadata[relPath][key] = obj[key];
+    for (let key in entry) {
+      metadata[relPath][key] = entry[key];
     }
-    await vscode.workspace.fs.writeFile(metadataUri, Buffer.from(JSON.stringify(metadata, null,
-    4), 'utf8'));
+    await vscode.workspace.fs.writeFile(
+        metadataUri, Buffer.from(JSON.stringify(metadata, null, 4), 'utf8'));
   }
 
-  // TODO: 덮어쓰기 추가
   public static async createDefault(uri: vscode.Uri, hash: string) {
     if (!vscode.workspace.workspaceFolders) {
       return;
@@ -121,128 +118,38 @@ export class Metadata {
     let metadata: any = await Metadata.getObj(hash);
     const relPath = vscode.workspace.asRelativePath(uri);
 
-    // If it doesn't have metadata, create default metadata
     if (!metadata) {
       metadata = {};
       await Metadata.setObj(hash, metadata);
-      // If it has metadata, exit
-    } else if (metadata[relPath] && Object.keys(metadata[relPath]).length !== 0) {
-      return;
+    }
+    if (!metadata[relPath]) {
+      metadata[relPath] = {};
     }
 
     const filename: any = relPath.split('/').pop();
     const stats: any =
         await getStats(vscode.Uri.joinPath(vscode.workspace.workspaceFolders[0].uri, relPath));
-    metadata[relPath] = {};
+
     metadata[relPath]['name'] = filename;
-    metadata[relPath]['file-extension'] = filename.split('.')[1];
+    metadata[relPath]['file-extension'] = filename.split('.').pop();
     metadata[relPath]['create-time'] = stats.birthtime;
     metadata[relPath]['modified-time'] = stats.mtime;
     metadata[relPath]['is-deleted'] = false;
     await Metadata.setObj(hash, metadata);
   }
 
-  /**
-   * Move metadata of the files and folders under the fromUri folder to the toUri folder
-   */
-
-  // TODO: move to event manager
-  // public static async moveUnderFolder(input: {[key: string]: any}) {
-  //   const fromUri = input['fromUri'];
-  //   const toUri = input['toUri'];
-
-  //   const pathToHash = await PathToHash.getInstance();
-  //   const toRelPath = vscode.workspace.asRelativePath(toUri);
-  //   const files = await vscode.workspace.findFiles(`${toRelPath}/**/*`);
-
-  //   for (let file of files) {
-  //     const toFilePath = file.path;
-  //     const fromFileUri = vscode.Uri.joinPath(
-  //         fromUri, toFilePath.substring(toFilePath.lastIndexOf(toUri.path) + toUri.path.length));
-  //     if (!pathToHash.isFile(fromFileUri)) {
-  //       await Metadata.moveUnderFolder({'fromUri': fromFileUri, 'toUri': file});
-  //     } else if (isValidFile(file)) {
-  //       await Metadata.move({'fromUri': fromFileUri, 'newUri': file});
-  //     }
-  //   }
-  // }
-
-  // TODO: move to event manager
-  // public static async move(input: {[key: string]: any}) {
-  //   const fromUri = input['fromUri'];
-  //   const toUri = input['toUri'];
-
-  //   const fromRelPath = vscode.workspace.asRelativePath(fromUri);
-  //   const toRelPath = vscode.workspace.asRelativePath(toUri);
-
-  //   if (isValidFile(fromUri) && !isValidFile(toUri)) {
-  //     // when the file is renamed from a valid file name to a invalid file name
-  //     // ex. a.log > a.txt
-  //     await Metadata.disable(fromUri);
-  //     return;
-  //   } else if (!isValidFile(fromUri) || !isValidFile(toUri)) {
-  //     return;
-  //   }
-
-  //   // 1. Get hash from pathToHash
-  //   const pathToHash = await PathToHash.getInstance();
-  //   const hash = pathToHash.get(fromUri);
-  //   if (hash === undefined) {
-  //     return;
-  //   }
-
-  //   // 2. Get metadata from the old path
-  //   const metadata = await Metadata.getObj(hash);
-  //   if (!metadata || !metadata[fromRelPath] || Object.keys(metadata[fromRelPath]).length === 0) {
-  //     // TODO: if there is no metadata, should we make a default metadata?
-  //     return;
-  //   }
-
-  //   // 3. Move metadata to the new path
-  //   metadata[fromRelPath]['name'] = toRelPath.split('/').pop();
-  //   metadata[toRelPath] = metadata[fromRelPath];
-  //   delete metadata[fromRelPath];
-  //   await Metadata.setObj(hash, metadata);
-
-  //   // 4. Update pathToHash
-  //   await pathToHash.add(toUri);
-  //   pathToHash.delete(fromUri);
-  // }
-
-  // deactivate all metadata under the folder
-  // TODO: move to event manager
-  // public static async disableUnderFolder(input: {[key: string]: any}) {
-  //   const uri = input['uri'];
-
-  //   // if it is a folder, deactivate all of its child files
-  //   const pathToHash = await PathToHash.getInstance();
-  //   const files = pathToHash.getFilesUnderFolder(uri);
-  //   for (let file of files) {
-  //     if (!pathToHash.isFile(file)) {
-  //       await Metadata.disableUnderFolder(file);
-  //     } else if (isValidFile(file)) {
-  //       await Metadata.disable(file);
-  //     }
-  //   }
-  // }
-
-  // TODO: enable disable, delete 외에는 event로 볼 것
-  public static async enable(uri: vscode.Uri, hash: string) {
-
-  }
-
-  // deactivate metadata
+  // NOTE When the deleted file is recovered, metadata is also recovered.
+  //    For that situation, metadata is not deleted but deactivated.
   public static async disable(uri: vscode.Uri, hash: string) {
-    const relPath = vscode.workspace.asRelativePath(uri);
     if (!isValidFile(uri)) {
       return;
     }
-
     // step 1. Find hash object with hash value
     const metadata = await Metadata.getObj(hash);
 
     // step 2. Check if the hash object has the deleted uri
-    if (metadata === undefined || !metadata[relPath] ||
+    const relPath = vscode.workspace.asRelativePath(uri);
+    if (metadata === undefined || metadata[relPath] === undefined ||
         Object.keys(metadata[relPath]).length !== 0) {
       return;
     }
@@ -258,4 +165,9 @@ export class Metadata {
     delete metadata[relPath];
     await Metadata.setObj(hash, metadata);
   }
+
+  // NOTE enable does not seem necessary because eventmanager does not call it.
+  //    When it's clear, this will be deleted.
+  // public static async enable(uri: vscode.Uri, hash: string) {
+  // }
 }
