f88c0faef75566f8717737fa8e0494f4685019ef
Semi Jeong <smjeong53@gmail.com>
2022-10-03 07:01:37 +0000

    fix: change saving format, change names of methods for saving information
    ONE-vscode-DCO-1.0-Signed-off-by: Semi Jeong <smjeong53@gmail.com>

--- src/Backend/One/LocalToolchain.ts
+++ src/Backend/One/LocalToolchain.ts
@@ -1,27 +1,30 @@
 
-import { OneStorage } from '../../OneExplorer/OneStorage';
-import { Backend } from '../Backend';
-import { Command } from '../Command';
-import { Compiler } from '../Compiler';
-import { Executor } from '../Executor';
-import { DeviceSpec } from '../Spec';
-import { Toolchain, ToolchainInfo, Toolchains } from '../Toolchain';
-import { Version } from '../Version';
-import * as fs from 'fs';
 import * as cp from 'child_process';
+import * as fs from 'fs';
+
+import {BuildInfo} from '../../MetadataManager/Metadata';
+import {Relation} from '../../MetadataManager/Relation';
+import {OneStorage} from '../../OneExplorer/OneStorage';
+import {Backend} from '../Backend';
+import {Command} from '../Command';
+import {Compiler} from '../Compiler';
+import {Executor} from '../Executor';
+import {DeviceSpec} from '../Spec';
+import {Toolchain, ToolchainInfo, Toolchains} from '../Toolchain';
+import {Version} from '../Version';
 
 const which = require('which');
 
-class LocalToolchain implements Backend {
-  private static _name = "dummy backend";
+export class LocalToolchain implements Backend {
+  private static _name = 'dummy backend';
 
   name(): string {
     return LocalToolchain._name;
   }
-  compiler(): Compiler | undefined {
+  compiler(): Compiler|undefined {
     return new DummyCompiler();
   }
-  executor(): Executor | undefined {
+  executor(): Executor|undefined {
     return new DummyExecutor();
   }
   executors(): Executor[] {
@@ -33,25 +36,25 @@ class DummyCompiler implements Compiler {
   getToolchainTypes(): string[] {
     return ['officially'];
   }
-  getToolchains(toolchainType: string, start: number, count: number): Toolchains {
+  getToolchains(_toolchainType: string, _start: number, _count: number): Toolchains {
     const array = new Array<Toolchain>();
-    const toolchainInfo = new ToolchainInfo("metadata toolchain", "Toolchain for metadata", new Version(1, 2, 3));
+    const toolchainInfo =
+        new ToolchainInfo('metadata toolchain', 'Toolchain for metadata', new Version(1, 2, 3));
     array.push(new MetadataToolchain(toolchainInfo));
     return array;
     // const array = new Array<Toolchain>();
-    // const toolchainInfo = new ToolchainInfo("dummy toolchain", "dummy!!!!", new Version(1, 2, 3));
-    // array.push(new DummyToolchain(toolchainInfo));
-    // return array;
+    // const toolchainInfo = new ToolchainInfo("dummy toolchain", "dummy!!!!", new Version(1, 2,
+    // 3)); array.push(new DummyToolchain(toolchainInfo)); return array;
   }
-  getInstalledToolchains(toolchainType: string): Toolchains {
+  getInstalledToolchains(_toolchainType: string): Toolchains {
     const array = new Array<Toolchain>();
-    const toolchainInfo = new ToolchainInfo("metadata toolchain", "Toolchain for metadata", new Version(1, 2, 3));
+    const toolchainInfo =
+        new ToolchainInfo('metadata toolchain', 'Toolchain for metadata', new Version(1, 2, 3));
     array.push(new MetadataToolchain(toolchainInfo));
     return array;
     // const array = new Array<Toolchain>();
-    // const toolchainInfo = new ToolchainInfo("dummy toolchain", "dummy!!!!", new Version(1, 2, 3));
-    // array.push(new DummyToolchain(toolchainInfo));
-    // return array;
+    // const toolchainInfo = new ToolchainInfo("dummy toolchain", "dummy!!!!", new Version(1, 2,
+    // 3)); array.push(new DummyToolchain(toolchainInfo)); return array;
   }
   prerequisitesForGetToolchains(): Command {
     return new Command('prerequisitesForGetToolchains');
@@ -60,27 +63,29 @@ class DummyCompiler implements Compiler {
 
 class DummyExecutor implements Executor {
   name(): string {
-    return "dummy executor"
+    return 'dummy executor';
   }
   getExecutableExt(): string[] {
     return ['.cfg', '.pb', '.tflite', 'onnx'];
   }
   toolchains(): Toolchains {
     const array = new Array<Toolchain>();
-    const toolchainInfo = new ToolchainInfo("metadata toolchain", "Toolchain for metadata", new Version(1, 2, 3));
+    const toolchainInfo =
+        new ToolchainInfo('metadata toolchain', 'Toolchain for metadata', new Version(1, 2, 3));
     array.push(new MetadataToolchain(toolchainInfo));
     return array;
   }
-  runInference(_modelPath: string, _options?: string[] | undefined): Command {
+  runInference(_modelPath: string, _options?: string[]|undefined): Command {
     return new Command('runInference');
   }
   require(): DeviceSpec {
-    return new DeviceSpec("hw", 'sw', undefined);
+    return new DeviceSpec('hw', 'sw', undefined);
   }
 }
 
 class MetadataToolchain extends Toolchain {
   run(_cfg: string): Command {
+    console.log('LocalToolcahin:run');
     // find onecc path (can find only if it is installed from debian pkg)
     let oneccPath = which.sync('onecc', {nothrow: true});
     if (oneccPath === null) {
@@ -90,35 +95,41 @@ class MetadataToolchain extends Toolchain {
 
     const cfgObj = OneStorage.getCfgObj(_cfg);
     console.log(cfgObj?.rawObj);
+    // const slashIdx = _cfg.lastIndexOf('/');
+    // const basePath = _cfg.substring(0, slashIdx == -1 ? 0 : slashIdx);
     if (cfgObj) {
       const cfgInfo: any = {};
       const enabledSteps = new Set<string>();
-      for(let [key, value] of Object.entries(cfgObj.rawObj)) {
+      for (let [key, value] of Object.entries(cfgObj.rawObj)) {
         const data: any = {};
         if (key === 'onecc') {
-          for(let [step, isEnabled] of Object.entries(value)) {
+          for (let [step, isEnabled] of Object.entries(value)) {
             if (isEnabled === 'True') {
               data[key] = true;
               // cfgInfo[key] = value;
               enabledSteps.add(step);
             }
           }
-        } else if(enabledSteps.has(key)) {
+        } else if (enabledSteps.has(key)) {
           /* eslint-disable */
+          // TODO: consider when the input/output path is a relative path
           const inputPath = value['input_path'];
           const outputPath = value['output_path'];
+          // const inputPath = (value['input_path'].charAt(0)==='/' ? '' : basePath) +
+          // value['input_path']; const outputPath = (value['input_path'].charAt(0)==='/' ? '' :
+          // basePath) + value['output_path'];
           // FIXME: consider when the input path and the output path is same
-          if(inputPath && outputPath && inputPath !== outputPath) {
-            // Metadata.setRelationInfoMap(outputPath, inputPath);
-            if(outputPath.split('.').pop() === 'circle') {
-              // Metadata.setRelationInfoMap(outputPath+'.log', outputPath);
+          if (inputPath && outputPath && inputPath !== outputPath) {
+            Relation.store(outputPath, inputPath);
+            if (outputPath.split('.').pop() === 'circle') {
+              Relation.store(outputPath + '.log', outputPath);
             }
             /* eslint-enable */
           }
-          for(let [k, v] of Object.entries(value)) {
+          for (let [k, v] of Object.entries(value)) {
             if (k !== 'input_path' && k !== 'output_path') {
               // while(k.)
-              data[k.replace(/-/gi, '-')] = v;
+              data[k.replace(/_/gi, '-')] = v;
             }
           }
         } else {
@@ -127,19 +138,19 @@ class MetadataToolchain extends Toolchain {
         cfgInfo[key] = data;
       }
       for (let product of cfgObj.getProducts) {
-        // Metadata.setBuildInfoMap(product.path, 'toolchain', this.info);
-        // Metadata.setBuildInfoMap(product.path, 'cfg', cfgInfo);
+        BuildInfo.set(product.path, 'toolchain', this.info);
+        BuildInfo.set(product.path, 'cfg', cfgInfo);
       }
     }
 
     const oneccRealPath = fs.realpathSync(oneccPath);
     const process = cp.spawnSync(oneccRealPath, ['--version']);
-    if(process.status === 0) {
+    if (process.status === 0) {
       const result = Buffer.from(process.stdout).toString();
       const oneccVersion = result.toString().split('\n')[0].split(' ')[2];
       if (cfgObj) {
         for (let product of cfgObj.getProducts) {
-          // Metadata.setBuildInfoMap(product.path, 'onecc', oneccVersion);
+          BuildInfo.set(product.path, 'onecc', oneccVersion);
         }
       }
     }
