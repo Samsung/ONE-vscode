e9d1e8ba0f4d0dc3e002f4612a71d56106c29b49
chshin59 <97646064+chshin59@users.noreply.github.com>
2022-10-03 16:10:10 +0900

    Draft/metadata to test (#101)
    * [Metadata Manager] Introduce Metadata
    
    This commit introduces metadata.ts, which contains functions that manipulate the metadata of file system.
    
    ONE-vscode-DCO-1.0-Signed-off-by: Changho Shin <chshin59@gmail.com>
    
    * [Metadata Manager] add BuildInfo class and update variable name changes
    
    This commit adds BuildInfo class and updates variable name changes.
    
    ONE-vscode-DCO-1.0-Signed-off-by: Changho Shin <chshin59@gmail.com>
    
    * [Metadata Manager] remove functions that do not fit metadata class
    
    This commit removes functions that do not fit metadata class.
    
    ONE-vscode-DCO-1.0-Signed-off-by: Changho Shin <chshin59@gmail.com>
    
    * [Metadata Manager] refactors function and variable name
    
    This commit refactors function and variable name.
    
    ONE-vscode-DCO-1.0-Signed-off-by: Changho Shin <chshin59@gmail.com>

---
+++ src/MetadataManager/Metadata.ts
@@ -0,0 +1,170 @@
+/*
+ * Copyright (c) 2022 Samsung Electronics Co., Ltd. All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as fs from 'fs';
+import * as vscode from 'vscode';
+
+import {ToolchainInfo} from '../Backend/Toolchain';
+import {obtainWorkspaceRoot} from '../Utils/Helpers';
+
+import {isValidFile} from './Utils';
+
+type BuildInfoKeys = 'onecc'|'toolchain'|'cfg';
+
+interface BuildInfoObj {
+  'onecc': string, 'toolchain': ToolchainInfo|undefined, 'cfg': any
+}
+
+export class BuildInfo {
+  private static _map = new Map<string, BuildInfoObj>();
+
+  public static set(path: string, key: BuildInfoKeys, value: any) {
+    const relPath = vscode.workspace.asRelativePath(path);
+    let info = BuildInfo._map.get(relPath);
+    if (info === undefined) {
+      info = {onecc: '', toolchain: undefined, cfg: undefined};
+      BuildInfo._map.set(relPath, info);
+    }
+    info[key] = value;
+  }
+
+  public static save(metaEntry: any, uri: vscode.Uri) {
+    const path = vscode.workspace.asRelativePath(uri);
+    const info = BuildInfo._map.get(path);
+    if (info) {
+      metaEntry['onecc-version'] = info['onecc'];
+      metaEntry['toolchain-version'] = info['toolchain'] ?.version ?.str();
+      metaEntry['cfg-settings'] = info['cfg'];
+    }
+    BuildInfo._map.delete(path);
+  }
+}
+
+export class Metadata {
+  constructor() {}
+
+  public static async getObj(hash: string) {
+    if (!vscode.workspace.workspaceFolders) {
+      return;
+    }
+
+    const jsonUri = vscode.Uri.joinPath(
+        vscode.workspace.workspaceFolders[0].uri,
+        `.meta/hash_objects/${hash.substring(0, 2)}/${hash.substring(2)}.json`);
+
+    if (fs.existsSync(jsonUri.fsPath)) {
+      return JSON.parse(Buffer.from(await vscode.workspace.fs.readFile(jsonUri)).toString());
+    }
+    // TODO: Error Handling
+    return undefined;
+  }
+
+  public static async setObj(hash: string, obj: object) {
+    const workspaceroot = obtainWorkspaceRoot();
+    const jsonUri = vscode.Uri.joinPath(
+        vscode.Uri.file(workspaceroot),
+        `.meta/hash_objects/${hash.substring(0, 2)}/${hash.substring(2)}.json`);
+    await vscode.workspace.fs.writeFile(jsonUri, Buffer.from(JSON.stringify(obj, null, 4), 'utf8'));
+  }
+
+  public static async getEntry(uri: vscode.Uri, hash: string) {
+    const metaObj = await Metadata.getObj(hash);
+    const relPath = vscode.workspace.asRelativePath(uri);
+    if (metaObj && metaObj[relPath] && Object.keys(metaObj[relPath]).length !== 0) {
+      return metaObj[relPath];
+    }
+    // TODO: Error Handling
+    return undefined;
+  }
+
+  public static async setEntry(uri: vscode.Uri, hash: string, entry: any) {
+    const workspaceroot = obtainWorkspaceRoot();
+    const jsonUri = vscode.Uri.joinPath(
+        vscode.Uri.file(workspaceroot),
+        `.meta/hash_objects/${hash.substring(0, 2)}/${hash.substring(2)}.json`);
+
+    const relPath = vscode.workspace.asRelativePath(uri);
+    const metaObj = await Metadata.getObj(hash);
+    if (!metaObj[relPath]) {
+      metaObj[relPath] = {};
+    }
+    for (let key in entry) {
+      metaObj[relPath][key] = entry[key];
+    }
+    await vscode.workspace.fs.writeFile(
+        jsonUri, Buffer.from(JSON.stringify(metaObj, null, 4), 'utf8'));
+  }
+
+  public static async createDefault(uri: vscode.Uri, hash: string) {
+    if (!vscode.workspace.workspaceFolders) {
+      return;
+    }
+
+    let metaObj: any = await Metadata.getObj(hash);
+    const relPath = vscode.workspace.asRelativePath(uri);
+
+    if (metaObj === undefined) {
+      metaObj = {};
+      await Metadata.setObj(hash, metaObj);
+    }
+    if (metaObj[relPath] === undefined) {
+      metaObj[relPath] = {};
+    }
+
+    const filename: any = relPath.split('/').pop();
+    const stats: any = fs.statSync(uri.fsPath);
+
+    metaObj[relPath]['name'] = filename;
+    metaObj[relPath]['file-extension'] = filename.split('.').pop();
+    metaObj[relPath]['create-time'] = stats.birthtime;
+    metaObj[relPath]['modified-time'] = stats.mtime;
+    metaObj[relPath]['is-deleted'] = false;
+    await Metadata.setObj(hash, metaObj);
+  }
+
+  // NOTE When the deleted file is recovered, metadata is also recovered.
+  //    For that situation, metadata is not deleted but deactivated.
+  public static async disable(uri: vscode.Uri, hash: string) {
+    if (!isValidFile(uri)) {
+      return;
+    }
+    // step 1. Find hash object with hash value
+    const metaObj = await Metadata.getObj(hash);
+
+    // step 2. Check if the hash object has the deleted uri
+    const relPath = vscode.workspace.asRelativePath(uri);
+    if (metaObj === undefined || metaObj[relPath] === undefined ||
+        Object.keys(metaObj[relPath]).length === 0) {
+      return;
+    }
+
+    // step 3. deactivate (set 'is_deleted') that path.
+    metaObj[relPath]['is-deleted'] = true;
+    await Metadata.setObj(hash, metaObj);
+  }
+
+  public static async delete(uri: vscode.Uri, hash: string) {
+    const metaObj = await Metadata.getObj(hash);
+    const relPath = vscode.workspace.asRelativePath(uri);
+    delete metaObj[relPath];
+    await Metadata.setObj(hash, metaObj);
+  }
+
+  // NOTE enable does not seem necessary because eventmanager does not call it.
+  //    When it's clear, this will be deleted.
+  // public static async enable(uri: vscode.Uri, hash: string) {
+  // }
+}
