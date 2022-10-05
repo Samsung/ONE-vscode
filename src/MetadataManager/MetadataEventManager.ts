41237116e1b52338bf75c8b4c764f2904aec41d7
Kukhee Lee <98148753+khlee025@users.noreply.github.com>
2022-10-05 13:38:17 +0900

    [Metadata Manager] Introduce Metadata Event Manager (#1313)
    * [Metadata Manager] Introduce Metadata Event Manager
    
    This commit adds metadata event manager
    
    ONE-vscode-DCO-1.0-Signed-off-by: Kukhee Lee <khlee025@gmail.com>
    
    * fix typo using lint
    
    This commit fixes typo using lint.
    
    * add readme.md to metadata manager
    
    This commit adds readme.md to metadata manager
    
    * Fix typo
    
    This commit fixes typo
    
    * fix access modifier
    
    This commit fixes access modifier
    
    * Update src/MetadataManager/MetadataEventManager.ts
    
    Co-authored-by: Seok Namkoong <seok9311@naver.com>
    
    * Update src/MetadataManager/MetadataEventManager.ts
    
    Co-authored-by: Dayoung Lee <dayg502@gmail.com>
    
    * Update src/MetadataManager/MetadataEventManager.ts
    
    Co-authored-by: Dayoung Lee <dayg502@gmail.com>
    
    * Update MetadataEventManager.ts
    
    * fix typo
    
    This commit fixes typo.
    
    Co-authored-by: Kukhee Lee <98148753+Cookie-Rook@users.noreply.github.com>
    Co-authored-by: Seok Namkoong <seok9311@naver.com>
    Co-authored-by: Dayoung Lee <dayg502@gmail.com>

---
+++ src/MetadataManager/MetadataEventManager.ts
@@ -0,0 +1,170 @@
+/*
+ * Copyright (c) 2022 Samsung Electronics Co., Ltd. All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import * as vscode from 'vscode';
+
+import {Balloon} from '../Utils/Balloon';
+import {obtainWorkspaceRoot} from '../Utils/Helpers';
+import {Logger} from '../Utils/Logger';
+
+class MetadataEventQueue {
+  private inProgress: boolean = false;
+  private queue: {method: Function, input: {[key: string]: any}}[] = [];
+
+  constructor() {
+    this.queue = [];
+  }
+
+  enqueue(method: any, input: {[key: string]: any}): void {
+    this.queue.push({method: method, input: input});
+    this.autoAction();
+  }
+
+  front() {
+    return this.queue[0];
+  }
+
+  dequeue() {
+    this.queue.shift();
+  }
+
+  clear() {
+    this.inProgress = false;
+    this.queue = [];
+  }
+
+  isEmpty() {
+    return this.queue.length === 0;
+  }
+
+  autoAction() {
+    if (this.inProgress === false) {
+      this.inProgress = true;
+      this.action();
+    }
+  }
+
+  async action() {
+    await this.front().method();
+
+    this.dequeue();
+
+    if (this.isEmpty()) {
+      this.clear();
+    } else {
+      this.action();
+    }
+  }
+}
+
+class MetadataEventBuffer {
+  private _queue = new MetadataEventQueue();
+  constructor() {}
+  public setEvent(request: any, input: {[key: string]: any}) {
+    this._queue.enqueue(() => {
+      return new Promise(resolve => {
+        if (Object.keys(input).length === 0) {
+          request().then((res: any) => resolve(res));
+        } else {
+          request(input).then((res: any) => resolve(res));
+        }
+      });
+    }, input);
+  }
+}
+
+
+/* istanbul ignore next */
+export class MetadataEventManager {
+  private fileWatcher = vscode.workspace.createFileSystemWatcher(`**/*`);  // glob pattern
+  public static eventBuffer = new MetadataEventBuffer();
+  // Used to communicate between events when file is renamed or moved
+  public static didCreateUri: vscode.Uri|undefined = undefined;
+
+
+  public static register(context: vscode.ExtensionContext) {
+    let workspaceRoot: vscode.Uri|undefined = undefined;
+
+    try {
+      workspaceRoot = vscode.Uri.file(obtainWorkspaceRoot());
+      Logger.info('OneExplorer', `workspace: ${workspaceRoot.fsPath}`);
+    } catch (e: unknown) {
+      if (e instanceof Error) {
+        if (e.message === 'Need workspace') {
+          Logger.info('OneExplorer', e.message);
+        } else {
+          Logger.error('OneExplorer', e.message);
+          Balloon.error('Something goes wrong while setting workspace.', true);
+        }
+      } else {
+        Logger.error('OneExplorer', 'Unknown error has been thrown.');
+      }
+    }
+
+    const manager = new MetadataEventManager();
+
+    let registrations = [
+      manager.fileWatcher.onDidChange(
+          async () => {
+              // TO BE IMPLEMENTED.
+          }),
+      vscode.workspace.onDidDeleteFiles(
+          async () => {
+              // TO BE IMPLEMENTED.
+          }),
+      manager.fileWatcher.onDidDelete(
+          async () => {
+              // TO BE IMPLEMENTED.
+          }),
+      manager.fileWatcher.onDidCreate(
+          async () => {
+              // TO BE IMPLEMENTED.
+          }),
+    ];
+
+    registrations.forEach(disposable => context.subscriptions.push(disposable));
+    // TO BE DONE.
+  }
+
+  async resetDidCreateUri() {
+    MetadataEventManager.didCreateUri = undefined;
+  }
+
+  async changeFileEvent(_input: {[key: string]: any}): Promise<void> {
+    // TO BE IMPLEMENTED
+  }
+
+  async createDirEvent(_input: {[key: string]: any}) {
+    // TO BE IMPLEMENTED
+  }
+
+  public static async createFileEvent(_input: {[key: string]: any}) {
+    // TO BE IMPLEMENTED
+  }
+
+  async deleteDirEvent(_input: {[key: string]: any}) {
+    // TO BE IMPLEMENTED
+  }
+
+  public static async deleteFileEvent(_input: {[key: string]: any}) {
+    // TO BE IMPLEMENTED
+  }
+  async moveDirEvent(_input: {[key: string]: any}) {
+    // TO BE IMPLEMENTED
+  }
+  public static async moveFileEvent(_input: {[key: string]: any}) {
+    // TO BE IMPLEMENTED
+  }
+}
