8dad6024cfd6e067f4ca45a4fbd731cb23ab06ad
SaeHie Park <saehie.park@gmail.com>
2022-10-21 11:27:59 +0900

    [media/CircleGraph] Visquv export style (#1411)
    This will apply Visquv styles in export.
    
    ONE-vscode-DCO-1.0-Signed-off-by: SaeHie Park <saehie.park@gmail.com>

--- media/CircleGraph/view.js
+++ media/CircleGraph/view.js
@@ -1004,6 +1004,27 @@ view.View = class {
         }
     }
 
+    applyStyleSheetVisq(element) {
+        if (this._host._mode === viewMode.visq) {
+            let rules = [];
+            for (const styleSheet of this._host.document.styleSheets) {
+                if (styleSheet.title === 'visq_style') {
+                    Array.prototype.push.apply(rules, styleSheet.cssRules);
+                }
+            }
+            const nodes = element.getElementsByTagName('*');
+            for (const node of nodes) {
+                for (const rule of rules) {
+                    if (node.matches(rule.selectorText)) {
+                        for (const item of rule.style) {
+                            node.style[item] = rule.style[item];
+                        }
+                    }
+                }
+            }
+        }
+    }
+
     export(file) {
         const lastIndex = file.lastIndexOf('.');
         const extension = (lastIndex !== -1) ? file.substring(lastIndex + 1) : '';
@@ -1011,6 +1032,7 @@ view.View = class {
             const canvas = this._getElementById('canvas');
             const clone = canvas.cloneNode(true);
             this.applyStyleSheet(clone, 'view-grapher.css');
+            this.applyStyleSheetVisq(clone);
             clone.setAttribute('id', 'export');
             clone.removeAttribute('viewBox');
             clone.removeAttribute('width');
