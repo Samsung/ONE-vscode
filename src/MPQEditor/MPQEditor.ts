0e21f6230e2c61fd2389bfebcb97bff80ffda633
s.malakhov <s.malakhov@partner.samsung.com>
2023-03-30 12:40:45 +0300

    [MPQEditor] Introduce MPQEditor
    This commit introduces MPQEditor for manual mixed precision quantization task.
    
    ONE-vscode-DCO-1.0-Signed-off-by: s.malakhov <s.malakhov@partner.samsung.com>

---
+++ src/MPQEditor/MPQEditor.ts
@@ -0,0 +1,120 @@
+/*
+ * Copyright (c) 2023 Samsung Electronics Co., Ltd. All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as vscode from "vscode";
+
+import { getNonce } from "../Utils/external/Nonce";
+import { getUri } from "../Utils/external/Uri";
+import { MPQData } from "./MPQData";
+
+export class MPQEditorProvider implements vscode.CustomTextEditorProvider {
+  public static readonly viewType = "one.editor.mpq";
+  public static readonly fileExtension = ".mpq.json";
+
+  private _disposables: vscode.Disposable[] = [];
+  private _mpqDataMap: any = {};
+
+  /**
+   * register MPQEditorProvider and its commands
+   */
+  public static register(context: vscode.ExtensionContext): void {
+    const provider = new MPQEditorProvider(context);
+    const registrations = [
+      vscode.window.registerCustomEditorProvider(
+        MPQEditorProvider.viewType,
+        provider,
+        {
+          webviewOptions: { retainContextWhenHidden: true },
+        }
+      ),
+      // Add command registration here
+    ];
+
+    registrations.forEach((disposable) =>
+      context.subscriptions.push(disposable)
+    );
+  }
+
+  constructor(private readonly context: vscode.ExtensionContext) {}
+
+  /**
+   * Called when custom editor is opened.
+   */
+  public async resolveCustomTextEditor(
+    document: vscode.TextDocument,
+    webviewPanel: vscode.WebviewPanel,
+    _token: vscode.CancellationToken
+  ): Promise<void> {
+    this._mpqDataMap[document.uri.toString()] = new MPQData();
+    await this.initWebview(document, webviewPanel);
+  }
+
+  /**
+   * Get the static html used for the editor webviews.
+   */
+  private async initWebview(
+    document: vscode.TextDocument,
+    webviewPanel: vscode.WebviewPanel
+  ): Promise<void> {
+    let webview: vscode.Webview = webviewPanel.webview;
+
+    webview.options = {
+      enableScripts: true,
+    };
+
+    const nonce = getNonce();
+    const scriptUri = getUri(webview, this.context.extensionUri, [
+      "media",
+      "MPQEditor",
+      "index.js",
+    ]);
+    const styleUri = getUri(webview, this.context.extensionUri, [
+      "media",
+      "MPQEditor",
+      "style.css",
+    ]);
+    const codiconUri = getUri(webview, this.context.extensionUri, [
+      "node_modules",
+      "@vscode",
+      "codicons",
+      "dist",
+      "codicon.css",
+    ]);
+    const toolkitUri = getUri(webview, this.context.extensionUri, [
+      "node_modules",
+      "@vscode",
+      "webview-ui-toolkit",
+      "dist",
+      "toolkit.js",
+    ]);
+    const htmlUri = vscode.Uri.joinPath(
+      this.context.extensionUri,
+      "media/MPQEditor/index.html"
+    );
+    let html = Buffer.from(
+      await vscode.workspace.fs.readFile(htmlUri)
+    ).toString();
+    html = html.replace(/\${nonce}/g, `${nonce}`);
+    html = html.replace(/\${webview.cspSource}/g, `${webview.cspSource}`);
+    html = html.replace(/\${scriptUri}/g, `${scriptUri}`);
+    html = html.replace(/\${toolkitUri}/g, `${toolkitUri}`);
+    html = html.replace(/\${cssUri}/g, `${styleUri}`);
+    html = html.replace(/\${codiconUri}/g, `${codiconUri}`);
+    webview.html = html;
+
+    //TODO process messages
+  }
+}
