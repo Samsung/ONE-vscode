fbb60a4e288c0d8f962f2ff69ff2045113a6db67
STak <59037261+profornnan@users.noreply.github.com>
2023-09-06 13:30:53 +0900

    [EdgeTPU] Add shell script for the prerequisites (#16)
    - Add the shell script that add PPA to get edgetpu-compiler package for EdgeTPUToolchain
    
    ONE-vscode-DCO-1.0-Signed-off-by: profornnan <profornnan@naver.com>

--- script/prerequisitesForGetToolchains.sh
+++ script/prerequisitesForGetToolchains.sh
@@ -3,6 +3,11 @@
 # This shell script add PPA to get onecc-docker package for ONEToolchain.
 
 PPA_NAME="ppa:one-compiler/onecc-docker"
-
 add-apt-repository -y ${PPA_NAME}
+
+# This shell script add PPA to get edgetpu-compiler package for EdgeTPUToolchain.
+
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://packages.cloud.google.com/apt coral-edgetpu-stable main" | sudo tee /etc/apt/sources.list.d/coral-edgetpu.list
+
 apt-get update -yqq
