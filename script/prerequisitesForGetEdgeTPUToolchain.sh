585977114f47a88b78a4e078024adcc97deacc29
profornnan <profornnan@naver.com>
2023-09-21 16:05:07 +0900

    [EdgeTPU] Add sudo privilege to prerequisites script
    Add sudo privilege to prerequisites script for edgetpu toolchain
    
    ONE-vscode-DCO-1.0-Signed-off-by: profornnan <profornnan@naver.com>

--- script/prerequisitesForGetEdgeTPUToolchain.sh
+++ script/prerequisitesForGetEdgeTPUToolchain.sh
@@ -5,4 +5,4 @@
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 echo "deb https://packages.cloud.google.com/apt coral-edgetpu-stable main" | sudo tee /etc/apt/sources.list.d/coral-edgetpu.list
 
-apt-get update -yqq
+sudo apt-get update -yqq
