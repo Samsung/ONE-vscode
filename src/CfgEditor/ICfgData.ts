3d0c3e636ff19d617040161a5826dc0472c1d309
Hyeon-Uk <rlagusdnr120@gmail.com>
2023-09-26 10:48:53 +0900

    [EdgeTPU] Refactor ICfgData.ts for modularization
    Child classes that inherit ICfgData.ts use all of the following functions equally.
    - getAsConfig()
    - getAsString()
    - isSame(textStringified : string)
    - sort()
    
    And, they have a common member variables. "_oneConfig", "_edgetpuConfig" and sections
    
    So, after changing ICfgData.ts from an interface to an abstract class,
    i created a constructor that received implementation and member variables from common methods.
    
    ONE-vscode-DCO-1.0-Signed-off-by: Hyeon-Uk <rlagusdnr120@gmail.com>

--- src/CfgEditor/ICfgData.ts
+++ src/CfgEditor/ICfgData.ts
@@ -13,18 +13,80 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import * as ini from "ini";
 
-export interface ICfgData {
-  // returns data decoded or parsed as object
-  getAsConfig(): any;
-  // returns data encoded or stringfied as string
-  getAsString(): string;
+export abstract class ICfgData {
+  //config can be undefined
+  private _config: any;
+  //section must be existed
+  private _section!: string[];
+
+  constructor(_config: any, _section: string[]) {
+    this._config = _config;
+    this._section = _section;
+  }
+
   // sets data with object decoded or parsed
-  setWithConfig(cfg: any): void;
+  abstract setWithConfig(cfg: any): void;
   // sets data with string encoded or stringfied
-  setWithString(text: string): void;
-  updateSectionWithKeyValue(section: string, key: string, value: string): void;
-  updateSectionWithValue(section: string, value: string): void;
-  isSame(textStringified: string): boolean;
-  sort(): void;
+  abstract setWithString(text: string): void;
+  abstract updateSectionWithKeyValue(
+    section: string,
+    key: string,
+    value: string
+  ): void;
+  abstract updateSectionWithValue(section: string, value: string): void;
+
+  // set cfgData's config
+  // only child class can use this method
+  protected setConfig(cfg: any): void {
+    this._config = cfg;
+  }
+
+  // returns data decoded or parsed as object
+  getAsConfig(): any {
+    return this._config;
+  }
+  // returns data encoded or stringfied as string
+  getAsString(): string {
+    return ini.stringify(this._config);
+  }
+
+  isSame(textStringified: string): boolean {
+    const iniDocument = ini.parse(textStringified);
+    for (const [sectionName, section] of Object.entries(this._config)) {
+      for (const [paramName, param] of Object.entries(section as any)) {
+        if (
+          iniDocument[sectionName] !== undefined &&
+          iniDocument[sectionName][paramName] === param
+        ) {
+          continue;
+        }
+        return false;
+      }
+    }
+    for (const [sectionName, section] of Object.entries(iniDocument)) {
+      for (const [paramName, param] of Object.entries(section as any)) {
+        if (
+          this._config[sectionName] !== undefined &&
+          this._config[sectionName][paramName] === param
+        ) {
+          continue;
+        }
+        return false;
+      }
+    }
+    return true;
+  }
+
+  sort(): void {
+    // cfg file is written along with the order of array elements
+    let sorted: any = {};
+    this._section.forEach((section) => {
+      if (this._config[section] !== undefined) {
+        sorted[section] = this._config[section];
+      }
+    });
+    this.setWithConfig(sorted);
+  }
 }
