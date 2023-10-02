ca2e4a80b98fe744c49c09cfe86b87c52e97662a
Bumsoo Ko <52022529+rhqjatn2398@users.noreply.github.com>
2023-10-02 21:54:57 +0900

    [EdgeTPU] Add EdgeTPUToolchian testing codes (#83)
    This commit adds the testing codes for EdgeTPUToolchain.
    
    ONE-vscode-DCO-1.0-Signed-off-by: Bumsoo Ko <rhqjatn2398@naver.com>

--- src/Tests/Backend/EdgeTPU/EdgeTPUToolchain.test.ts
+++ src/Tests/Backend/EdgeTPU/EdgeTPUToolchain.test.ts
@@ -20,10 +20,13 @@ import * as vscode from "vscode";
 import {
   EdgeTPUCompiler,
   EdgeTPUDebianToolchain,
+  EdgeTPUToolchain,
 } from "../../../Backend/EdgeTPU/EdgeTPUToolchain";
 import { ToolchainInfo } from "../../../Backend/Toolchain";
 import { Version } from "../../../Backend/Version";
 import { TestBuilder } from "../../TestBuilder";
+
+const edgeTPUBackendName = "EdgeTPU";
 
 const content = `
 [edgetpu-compile]
@@ -266,5 +269,34 @@ suite("EdgeTPUCompiler", function () {
         cmd
       );
     });
+  });
+
+  suite("EdgeTPUToolchain", function () {
+    suite("#constructor()", function () {
+      test("Create dummy EdgeTPUToolchain backend", function (pass) {
+        assert.doesNotThrow(() => new EdgeTPUToolchain());
+  
+        pass();
+        assert.ok(true);
+      });
+    });
+  
+    suite("#name()", function () {
+      test("returns backend name", function () {
+        const edgeTPUBackend = new EdgeTPUToolchain();
+        assert.strictEqual(edgeTPUBackend.name(), edgeTPUBackendName);
+      });
+    });
+  
+    suite("#compiler()", function () {
+      test("returns edgeTPUCompiler", function () {
+        const edgeTPUBackend = new EdgeTPUToolchain();
+        const edgeTPUCompiler = edgeTPUBackend.compiler();
+        assert.instanceOf(edgeTPUCompiler, EdgeTPUCompiler);
+      });
+    });
+  
+    // TODO
+    // Add test case for executor() and executors()
   });
 });
