b7cf5bcc2640860f09d051c2113de0745150270f
profornnan <profornnan@naver.com>
2023-10-05 19:32:26 +0900

    [EdgeTPU] Update OneCompiler to extends DebianCompiler
    Update OneCompiler to extends DebianCompiler
    
    ONE-vscode-DCO-1.0-Signed-off-by: profornnan <profornnan@naver.com>

--- src/Backend/One/OneToolchain.ts
+++ src/Backend/One/OneToolchain.ts
@@ -14,22 +14,21 @@
  * limitations under the License.
  */
 
-import * as cp from "child_process";
 import * as vscode from "vscode";
 
-import { pipedSpawnSync } from "../../Utils/PipedSpawnSync";
 import { Balloon } from "../../Utils/Balloon";
 import { Backend } from "../Backend";
 import { Command } from "../Command";
 import { Compiler } from "../Compiler";
 import { Executor } from "../Executor";
-import { PackageInfo, ToolchainInfo, Toolchains } from "../Toolchain";
+import { PackageInfo } from "../Toolchain";
 import {
   DebianArch,
   DebianRepo,
   DebianToolchain,
 } from "../ToolchainImpl/DebianToolchain";
 import { Version } from "../Version";
+import { DebianCompiler } from "../CompilerImpl/DebianCompiler";
 
 class OneDebianToolchain extends DebianToolchain {
   run(cfg: string): Command {
@@ -50,209 +49,21 @@ class OneDebianToolchain extends DebianToolchain {
   }
 }
 
-class OneCompiler implements Compiler {
-  private readonly toolchainTypes: string[];
-  private readonly toolchainName: string;
-  private readonly debianRepo: DebianRepo;
-  private readonly debianArch: DebianArch;
-
+class OneCompiler extends DebianCompiler {
   constructor() {
-    this.toolchainName = "onecc-docker";
-    this.toolchainTypes = ["latest"];
-
-    this.debianRepo = new DebianRepo(
-      "http://ppa.launchpad.net/one-compiler/onecc-docker/ubuntu",
-      "bionic",
-      "main"
+    super(
+      ["latest"],
+      "onecc-docker",
+      OneDebianToolchain,
+      [new PackageInfo("one-compiler", new Version(1, 21, 0))],
+      "prerequisitesForGetOneToolchain.sh",
+      new DebianRepo(
+        "http://ppa.launchpad.net/one-compiler/onecc-docker/ubuntu",
+        "bionic",
+        "main"
+      ),
+      DebianArch.amd64
     );
-    this.debianArch = DebianArch.amd64;
-  }
-
-  getToolchainTypes(): string[] {
-    return this.toolchainTypes;
-  }
-
-  parseVersion(version: string): Version {
-    if (!version.trim()) {
-      throw Error("Invalid version format.");
-    }
-
-    let _version = version;
-    let option = "";
-
-    const optionIndex = version.search(/[~+-]/);
-    if (optionIndex !== -1) {
-      option = version.slice(optionIndex);
-      _version = version.slice(0, optionIndex);
-    }
-
-    const splitedVersion = _version.split(".");
-
-    if (splitedVersion.length > 3) {
-      throw Error("Invalid version format.");
-    }
-
-    let major: number | string;
-    let minor: number | string;
-    let patch: number | string;
-
-    [major = "0", minor = "0", patch = "0"] = _version.split(".");
-
-    const epochIndex = major.search(/:/);
-    if (epochIndex !== -1) {
-      major = major.slice(epochIndex + 1);
-    }
-
-    major = Number(major);
-    minor = Number(minor);
-    patch = Number(patch);
-
-    if (isNaN(major) || isNaN(minor) || isNaN(patch)) {
-      throw Error("Invalid version format.");
-    }
-    return new Version(major, minor, patch, option);
-  }
-
-  getToolchains(
-    _toolchainType: string,
-    _start: number,
-    _count: number
-  ): Toolchains {
-    if (_toolchainType !== "latest") {
-      throw Error(`Invalid toolchain type : ${_toolchainType}`);
-    }
-
-    if (_start < 0) {
-      throw Error(`wrong start number: ${_start}`);
-    }
-    if (_count < 0) {
-      throw Error(`wrong count number: ${_count}`);
-    }
-    if (_count === 0) {
-      return [];
-    }
-
-    try {
-      cp.spawnSync(`apt-cache show ${this.toolchainName}`);
-    } catch (error) {
-      throw Error(`Getting ${this.toolchainName} package list is failed`);
-    }
-
-    let result;
-    try {
-      result = pipedSpawnSync(
-        "apt-cache",
-        ["madison", `${this.toolchainName}`],
-        { encoding: "utf8" },
-        "awk",
-        ['{printf $3" "}'],
-        { encoding: "utf8" }
-      );
-    } catch (error) {
-      throw Error(
-        `Getting ${this.toolchainName} package version list is failed`
-      );
-    }
-
-    if (result.status !== 0) {
-      return [];
-    }
-
-    const toolchainVersions: string = result.stdout.toString();
-    const versionList = toolchainVersions.trim().split(" ");
-
-    const availableToolchains = new Toolchains();
-    for (const version of versionList) {
-      const toolchainInfo = new ToolchainInfo(
-        this.toolchainName,
-        "Description: test",
-        this.parseVersion(version)
-      );
-
-      const toolchain = new OneDebianToolchain(
-        toolchainInfo,
-        this.debianRepo,
-        this.debianArch
-      );
-      availableToolchains.push(toolchain);
-    }
-
-    return availableToolchains;
-  }
-
-  getInstalledToolchains(_toolchainType: string): Toolchains {
-    if (_toolchainType !== "latest") {
-      throw Error(`Invalid toolchain type : ${_toolchainType}`);
-    }
-
-    let result;
-    try {
-      result = cp.spawnSync(
-        "dpkg-query",
-        [
-          "--show",
-          `--showformat='\${Version} \${Description}'`,
-          `${this.toolchainName}`,
-        ],
-        { encoding: "utf8" }
-      );
-    } catch (error) {
-      throw new Error(
-        `Getting installed ${this.toolchainName} package list is failed`
-      );
-    }
-
-    if (result.status !== 0) {
-      return [];
-    }
-
-    // NOTE
-    // The output format string of dpkg-query is '${Version} ${Description}'.
-    // To remove the first and last single quote character of output string, it slices from 1 to -1.
-    const installedToolchain: string = result.stdout.toString().slice(1, -1);
-
-    const descriptionIdx = installedToolchain.search(" ");
-    const versionStr = installedToolchain.slice(0, descriptionIdx).trim();
-    const description = installedToolchain.slice(descriptionIdx).trim();
-
-    // NOTE
-    // onecc-docker does not have any explict dependencies,
-    // but it has internally dependency to one-compiler and it is seen in depends.
-
-    // TODO
-    // onecc-docker's depends should be modified later so that we can read the one-compiler version.
-
-    const depends: Array<PackageInfo> = [
-      new PackageInfo("one-compiler", new Version(1, 21, 0)),
-    ];
-    const toolchainInfo = new ToolchainInfo(
-      this.toolchainName,
-      description,
-      this.parseVersion(versionStr),
-      depends
-    );
-    const toolchain = new OneDebianToolchain(
-      toolchainInfo,
-      this.debianRepo,
-      this.debianArch
-    );
-    return [toolchain];
-  }
-
-  prerequisitesForGetToolchains(): Command {
-    const extensionId = "Samsung.one-vscode";
-    const ext = vscode.extensions.getExtension(
-      extensionId
-    ) as vscode.Extension<any>;
-    const scriptPath = vscode.Uri.joinPath(
-      ext!.extensionUri,
-      "script",
-      "prerequisitesForGetToolchains.sh"
-    ).fsPath;
-
-    const cmd = new Command("/bin/sh", [`${scriptPath}`]);
-    cmd.setRoot();
-    return cmd;
   }
 }
 
