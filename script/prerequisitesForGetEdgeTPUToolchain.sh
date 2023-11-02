c15e04e74eedd8c0e572456d7d17e541766ebb45
Dayoung Lee <dayoung.lee@samsung.com>
2023-11-02 15:00:10 +0900

    Update script/prerequisitesForGetEdgeTPUToolchain.sh
    Co-authored-by: Jiyoung Giuliana Yun <wldudyun10@gmail.com>

--- script/prerequisitesForGetEdgeTPUToolchain.sh
+++ script/prerequisitesForGetEdgeTPUToolchain.sh
@@ -3,6 +3,6 @@
 # This shell script add PPA to get edgetpu-compiler package for EdgeTPUToolchain.
 
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-echo "deb https://packages.cloud.google.com/apt coral-edgetpu-stable main" | sudo tee /etc/apt/sources.list.d/coral-edgetpu.list
+echo "deb https://packages.cloud.google.com/apt coral-edgetpu-stable main" | tee /etc/apt/sources.list.d/coral-edgetpu.list
 
 sudo apt-get update -yqq
