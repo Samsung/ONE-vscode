c8de56b065c1330e9a409ed42094229d5b213f96
Dayoung Lee <dayoung.lee@samsung.com>
2023-11-02 15:09:53 +0900

    [EdgeTPU] Add EdgeTPUDebianToolchain (#1733)
    - implemented parsing the cfg file to construct command
    - used ini and fs packages to read cfg file
    
    ONE-vscode-DCO-1.0-Signed-off-by: Bumsoo Ko <rhqjatn2398@naver.com>
    
    Co-authored-by: Bumsoo Ko <52022529+rhqjatn2398@users.noreply.github.com>
    Co-authored-by: profornnan <profornnan@naver.com>

---
+++ src/Backend/EdgeTPU/EdgeTPUToolchain.ts
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2023 Samsung Electronics Co., Ltd. All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Command } from "../Command";
+import { DebianToolchain } from "../ToolchainImpl/DebianToolchain";
+
+import * as ini from "ini";
+import * as fs from "fs";
+import * as path from "path";
+import { Logger } from "../../Utils/Logger";
+
+class EdgeTPUDebianToolchain extends DebianToolchain {
+  run(cfg: string): Command {
+    let cmd = new Command("edgetpu_compiler");
+    var config = ini.parse(fs.readFileSync(cfg, "utf-8").trim());
+
+    if (config["edgetpu-compile"] === undefined) {
+      Logger.error(
+        "EdgeTPUDebianToolchain",
+        `The configuration file doesn't include 'edgetpu-compile' section.`
+      );
+
+      throw new Error(
+        `The configuration file doesn't include ''edgetpu-compile' section.`
+      );
+    }
+
+    let outDir = path.dirname(config["edgetpu-compile"]["output_path"]);
+    cmd.push("--out_dir");
+    cmd.push(outDir);
+
+    let intermediateTensors = config["edgetpu-compile"]["intermediate_tensors"];
+    if (intermediateTensors !== undefined) {
+      cmd.push("--intermediate_tensors");
+      cmd.push(intermediateTensors);
+    }
+
+    let showOperations = config["edgetpu-compile"]["show_operations"];
+    if (showOperations === "True") {
+      cmd.push("--show_operations");
+    }
+
+    let minRuntimeVersion = config["edgetpu-compile"]["min_runtime_version"];
+    if (minRuntimeVersion !== undefined) {
+      cmd.push("--min_runtime_version");
+      cmd.push(minRuntimeVersion);
+    }
+
+    let searchDelegate = config["edgetpu-compile"]["search_delegate"];
+    if (searchDelegate === "True") {
+      cmd.push("--search_delegate");
+    }
+
+    let delegateSearchStep = config["edgetpu-compile"]["delegate_search_step"];
+    if (delegateSearchStep !== undefined) {
+      cmd.push("--delegate_search_step");
+      cmd.push(delegateSearchStep);
+    }
+
+    let inputPath = config["edgetpu-compile"]["input_path"];
+    cmd.push(inputPath);
+
+    return cmd;
+  }
+}
+
+export { EdgeTPUDebianToolchain };
